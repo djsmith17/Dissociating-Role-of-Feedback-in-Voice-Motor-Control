adfcc1a369c683dae2553ce31055b17cc66a986b
djsmith <djsmith@bu.edu>
2017-02-16 11:23:51 -0500

    Making orderTrials more robust
    overhauling orderTrials to work a little more effeciently

--- Presentation/orderTrials.m
+++ Presentation/orderTrials.m
@@ -5,14 +5,22 @@ function trialType = orderTrials(numTrial, per)
 %the second or third trial in the set. This will generate enough sets for
 %all trials in 'numTrial'.
 
-r = rem(numTrial,per);
+setSize = 1/per;
+options = setSize -2 ; %Excluding first and last position in the set. 
 
 trialType = [];
-for ii = 1:(numTrial*per)
-    place = round(rand) + 2; %This will break if per!= 0.25 **Fix this**
-    set = zeros(1,(1/per));
-    set(place) = 1;
-    
-    trialType = cat(2, trialType, set);
-end
+numSets = numTrial*per;
+if round(numSets) ~= numSets
+    disp('ERROR: Give me a whole number of sets')
+elseif per >= 0.5
+    disp('ERROR: This pattern does not support 50% catch trials')
+else
+    for ii = 1:numSets
+        set = zeros(1, setSize);
+        place = round(rand) + options ;    
+        set(place) = 1;
+
+        trialType = cat(2, trialType, set);
+    end
+end  
 end
