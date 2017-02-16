26288fce35c858dbfee361402db3d56b8055f44c
djsmith <djsmith@bu.edu>
2017-02-16 14:10:34 -0500

    More effecient OrderTrials
    Added some comments, but now orderTrials works the way it is supposed
    to.

--- Presentation/orderTrials.m
+++ Presentation/orderTrials.m
@@ -1,12 +1,22 @@
 function trialType = orderTrials(numTrial, per)
-%This function organizes trials in sets based on variable 'per'. 
-%For example if per = 0.25, then a set of four trials will have 
-%three control trials, and one catch trial. The catch trial will always be 
-%the second or third trial in the set. This will generate enough sets for
-%all trials in 'numTrial'.
+%This function organizes trials in sets based on variable 'per'. The first
+%trial and the last trial of each set is always a control trial. A random
+%trial between the first and last trial will be a catch trial, with the
+%remaining trials of the set being control. The length of a set will depend
+%on the total number of trials (numTrial), and the percent of catch trials
+%(per)in the paradigm. For example if per = 0.25, then a set of trials will
+%contain four trials; three control trials, and one catch trial. 
+%The catch trial will always be either the second or third trial in the set.  
+%This will generate enough sets for all trials in 'numTrial'.
+
+%numTrial: The total number of trials 
+%per:      The percent (in decimal) of total trials that are catch trials.
+
+%trialType: A vector of length trialType of 0s and 1s representing the
+%order of control and catch trials. 
 
 setSize = 1/per;
-options = setSize -2 ; %Excluding first and last position in the set. 
+options = setSize - 3 ; %Excluding first and last pos in the set and pos 2. 
 
 trialType = [];
 numSets = numTrial*per;
@@ -17,7 +27,7 @@ elseif per >= 0.5
 else
     for ii = 1:numSets
         set = zeros(1, setSize);
-        place = round(rand) + options ;    
+        place = round(options*rand) + 2; %Always want to at least start at pos 2    
         set(place) = 1;
 
         trialType = cat(2, trialType, set);
