c6c1789f7583687574ece5b4584994dce20014cf
djsmith <djsmith@bu.edu>
2017-02-16 14:15:53 -0500

    Removed the baked in orderTrials functions
    Removed the baked in orderTrials functions

--- Presentation/AFPerturb.m
+++ Presentation/AFPerturb.m
@@ -180,23 +180,6 @@ elseif masking == 1
 end 
 end
 
-function trialType = orderTrialsLaced(numTrial, per)
-%This function organizes trials in sets based on variable 'per'. 
-%For example if per = 0.25, then a set of four trials will have 
-%three control trials, and one catch trial. The catch trial will always be 
-%the second or third trial in the set. This will generate enough sets for
-%all trials in 'numTrial'.
-
-trialType = [];
-for ii = 1:(numTrial*per)
-    place = round(rand) + 2; %This will break if per!= 0.25 **Fix this**
-    set = zeros(1,(1/per));
-    set(place) = 1;
-    
-    trialType = cat(2, trialType, set);
-end
-end
-
 function plotPerturb(s,lenT,sig)
 
 t = (0:1:lenT-1)/s.Rate;
