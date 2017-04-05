c5554e0d8d3c838786cc99fff5a337097e1d6fc9
djsmith <djsmith@bu.edu>
2017-04-05 00:49:29 -0400

    Removed a ton of old parts
    Removed a lot of the sturcture from before. Now to clean up how dist is
    changed. Will test shortly

--- Presentation/dfRunf0Acuity.m
+++ Presentation/dfRunf0Acuity.m
@@ -60,8 +60,7 @@ pause
 
 % Loop until we get the necessary # of reversals
 while reversals < MaxReversals 
-    trial = trial + 1; % advance the trial every time we go through this loop
-    
+    trial = trial + 1; % advance the trial every time we go through this loop 
     RandomCatchTrial = randperm(randdiff, 1); % 1/8 if the time it is the same and 50% it is different
     
     % Create the comparison stimulus
@@ -112,123 +111,65 @@ while reversals < MaxReversals
     
     match = isequal(diffTokens, response); %Was the subject correct?
     matches = cat(1, matches, match);
-    
-    for Phase = 1:2 %phase 1 plays first then phase 2
-        if (trialType == 3) || (trialType == 1 && Phase == 1) || (trialType == 2 && Phase == 2)
-           %if catchtrial, both phases play reference
-           %OR if different trial order 1 AND first phase play reference
-           %OR if different trial order 2 AND second phase play reference
-            
-           if trialType == 1 
-               distVals = [distVals dist]; %saves all the dist values BEFORE the person hears them/makes a decisions
-           end
 
-           soundwav = soundwav_Def; %plays the baseline - doesn't change
-        else % makes the comparision stimuli
-            if trialType == 2
-                distVals = [distVals dist];
-            end   %saves all the dist values BEFORE the person hears them/makes a decisions
-
-            newf = freqDef*2^(dist/12);
-            soundwav = cos(time*2*pi*newf);
-        end
-
-       % Window the beginning and end of the stimuli slightly to avoid clicks
-        soundwav = soundwav .* taper';
-
-        if Phase == 1
-            disp(['Hit return to begin next trial'])% what the users sees on the screen
-            pause
-        end
-
-        sound(.05*soundwav*10^(-6/20), fs); %65 dB on MOTU equipment
-
-        if Phase == 1
-            pause(2) % Play the two sounds in succession (with a short pause between)
-        end
-
-        if Phase == 2
-            fprintf('Same (0) or Different (1)?'); % ask them if they were the same or different
-            YourAnswer = getkey; %getkey is a function that finds the value of the next keypress, getkey MUST be in the same folder as this code
-            fprintf(' %c\n', YourAnswer) %displays the key that was entered
-            
-            if YourAnswer == 49 % key press = 1
-                response(trial) = 1; %the response was 1 (different)
-            elseif YourAnswer == 48 % key press = 0
-                response(trial) = 0; %the response was 0 (same)
-            end
-
-            % Determine if the user was correct or incorrect
-
-            % If it is a catch trial, then isDiff will be 0, and 
-            % a response of "Same" (0) will be a match. 
-
-            % If it is not a catch trial, isDiff will be 1, and
-            % a response of "Different" (1) will be a match.
-
-            % In all other cases the trial will not be a match
-            match(trial) = isequal(diffTokens,(response(trial))); 
-            
-            if dist < 0
-              dist     = 0.02;
-              upstep   = 0.01;
-            else
-              if trial > 10 %reduce step size
-                  upstep = 0.1;
-              elseif trial > 20 %reduce step size again
-                  upstep = 0.05;
-              elseif trial > 30 %reduce step size again (last time)
-                  upstep = 0.02;
-              end
-            end
-            
-            downstep = upstep/udRatio;
-
-            % Adaptively adjust dist as required by 2-down, 1-up
-            if (trial > 1) %nothing happens on trial 1, it does not get included into 2 up and 1 down calculations
-                if (match(trial)) %this is if match = 1, so they were correct this trial
-                    if (correctInARow == 1) %this means they were correct last trial as well
-                        dist = dist - downstep; %they now have got 2 correct in a row, so move the dist down to make it closer to the reference, aka harder
-
-                        if (changeDirection == 1)% if it did change direction count a reversal, record the dist value and then note that it already changed directions
-                            reversals = reversals + 1; % counting the reversals
-                            revValues = [revValues dist]; %recording the revValues of the dist that JUST happened 
-                            changeDirection = -1; %nothing that it just changed directions
-                        end
-                        correctInARow = 0; % reset the # in a row counter after you have changed the dist
-                    else
-                        dist = dist; % no change in dist
-                        % This is the first correct in a row.  No change in dist
-                        correctInARow = 1; % mark our # in a row counter
-                    end
-                else
-                    % The subject got the last trial wrong -> increase delta
-                    dist = dist + upstep;
-                    if (changeDirection == -1)
-                        % This was a reversal in direction of changes
-                        reversals = reversals + 1;
-                        revValues = [revValues dist];
-                        changeDirection = 1;
-                    end
-                    correctInARow = 0;
-                end
-            else
-                % This was the first trial of the experiments - do nothing
-                dist(trial) = dist(trial);
-            end
-
-            if dist < 0
-                dist = .02; %disp('it was negative!')
-            end
-            if trial > 84
-                reversals = MaxReversals + 1;
-            end
-        
-            ResultMatrix = [distVals', matches', responses', trialTypes'];
-            save(dirs.SavFileDir, 'ResultMatrix', 'revValues');
+    if dist < 0
+        dist     = 0.02;
+        upstep   = 0.01;
+    else
+        if trial > 10 %reduce step size
+            upstep = 0.1;
+        elseif trial > 20 %reduce step size again
+            upstep = 0.05;
+        elseif trial > 30 %reduce step size again (last time)
+            upstep = 0.02;
         end
     end
+            
+    downstep = upstep/udRatio;
+
+    % Adaptively adjust dist as required by 2-down, 1-up
+    if (trial > 1) %nothing happens on trial 1, it does not get included into 2 up and 1 down calculations
+        if (match(trial)) %this is if match = 1, so they were correct this trial
+            if (correctInARow == 1) %this means they were correct last trial as well
+                dist = dist - downstep; %they now have got 2 correct in a row, so move the dist down to make it closer to the reference, aka harder
+
+                if (changeDirection == 1)% if it did change direction count a reversal, record the dist value and then note that it already changed directions
+                    reversals = reversals + 1; % counting the reversals
+                    revValues = [revValues dist]; %recording the revValues of the dist that JUST happened 
+                    changeDirection = -1; %nothing that it just changed directions
+                end
+                correctInARow = 0; % reset the # in a row counter after you have changed the dist
+            else
+                dist = dist; % no change in dist
+                % This is the first correct in a row.  No change in dist
+                correctInARow = 1; % mark our # in a row counter
+            end
+        else
+            % The subject got the last trial wrong -> increase delta
+            dist = dist + upstep;
+            if (changeDirection == -1)
+                % This was a reversal in direction of changes
+                reversals = reversals + 1;
+                revValues = [revValues dist];
+                changeDirection = 1;
+            end
+            correctInARow = 0;
+        end
+    else
+        % This was the first trial of the experiments - do nothing
+        dist(trial) = dist(trial);
+    end
+
+    if dist < 0
+        dist = .02; %disp('it was negative!')
+    end
+    if trial > 84
+        reversals = MaxReversals + 1;
+    end
 end
+
+ResultMatrix = [distVals', matches', responses', trialTypes'];
+save(dirs.SavFileDir, 'ResultMatrix', 'revValues');
 
 Last5Mean = mean(revValues(end-5:end));
 end
