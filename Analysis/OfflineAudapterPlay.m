5deb1ceacde3fef4cb3a33d28527ae3178f54ef4
Dante Smith <djsmith@bu.edu>
2017-04-27 02:40:31 -0400

    offline noe makes one file for all trials
    quick fixes. should be all set. testing it now

--- Analysis/OfflineAudapterPlay.m
+++ Analysis/OfflineAudapterPlay.m
@@ -7,6 +7,9 @@ if isempty(varargin)
 else
 end
 
+collectNewData         = 1; %Boolean
+sv2F                   = 1; %Boolean
+
 %Experiment Configurations
 expParam.project       = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control';
 expParam.expType       = 'Auditory Perturbation_Perceptual';
@@ -43,113 +46,107 @@ end
 if exist(dirs.SavResultsDir, 'dir') == 0
     mkdir(dirs.SavResultsDir)
 end
+if collectNewData == 1
+    %Paradigm Configurations
+    expParam.sRate              = 48000;  % Hardware sampling rate (before downsampling)
+    expParam.downFact           = 3;
+    expParam.sRateAnal          = expParam.sRate/expParam.downFact; %Everything get automatically downsampled! So annoying
+    expParam.frameLen           = 96;  % Before downsampling
+    expParam.audioInterfaceName = 'MOTU MicroBook'; %'ASIO4ALL' 'Komplete'
 
-%Paradigm Configurations
-expParam.sRate              = 48000;  % Hardware sampling rate (before downsampling)
-expParam.downFact           = 3;
-expParam.sRateAnal          = expParam.sRate/expParam.downFact; %Everything get automatically downsampled! So annoying
-expParam.frameLen           = 96;  % Before downsampling
-expParam.audioInterfaceName = 'MOTU MicroBook'; %'ASIO4ALL' 'Komplete'
+    %Set up Audapter
+    Audapter('deviceName', expParam.audioInterfaceName);
+    Audapter('setParam', 'downFact', expParam.downFact, 0);
+    Audapter('setParam', 'sRate', expParam.sRateAnal, 0);
+    Audapter('setParam', 'frameLen', expParam.frameLen / expParam.downFact, 0);
+    p = getAudapterDefaultParams(expParam.gender);
 
-%Set up Audapter
-Audapter('deviceName', expParam.audioInterfaceName);
-Audapter('setParam', 'downFact', expParam.downFact, 0);
-Audapter('setParam', 'sRate', expParam.sRateAnal, 0);
-Audapter('setParam', 'frameLen', expParam.frameLen / expParam.downFact, 0);
-p = getAudapterDefaultParams(expParam.gender);
+    %Set up Parameters to control NIDAQ and Perturbatron
+    s = initNIDAQ(expParam.trialLen, 'Dev2');
+    expParam.sRateQ = s.Rate; %save the sampling rate of the NIDAQ
 
-%Set up Parameters to control NIDAQ and Perturbatron
-s = initNIDAQ(expParam.trialLen, 'Dev2');
-expParam.sRateQ = s.Rate; %save the sampling rate of the NIDAQ
+    %Set up OST and PCF Files
+    expParam.ostFN = fullfile(dirs.Prelim, 'AFPerturbOST.ost'); check_file(expParam.ostFN);
+    expParam.pcfFN = fullfile(dirs.Prelim, 'AFPerturbPCF.pcf'); check_file(expParam.pcfFN);
 
-%Set up OST and PCF Files
-expParam.ostFN = fullfile(dirs.Prelim, 'AFPerturbOST.ost'); check_file(expParam.ostFN);
-expParam.pcfFN = fullfile(dirs.Prelim, 'AFPerturbPCF.pcf'); check_file(expParam.pcfFN);
+    %Should return variables of InflaRespRoute and tStep. 
+    %Recorded from previous experiments
+    dirs.InflaRespFile = fullfile(dirs.SavData, expParam.subject, [expParam.subject '_AveInflaResp.mat']);
+    try
+        load(dirs.InflaRespFile);
+    catch me
+        fprintf('\nSubject Data does not exist at %s \n', dirs.InflaRespFile)
+    end
 
-%Should return variables of InflaRespRoute and tStep. 
-%Recorded from previous experiments
-dirs.InflaRespFile = fullfile(dirs.SavData, expParam.subject, [expParam.subject '_AveInflaResp.mat']);
-try
-    load(dirs.InflaRespFile);
-catch me
-    fprintf('\nSubject Data does not exist at %s \n', dirs.InflaRespFile)
-end
+    [expParam, p]      = dfSetAudFB(expParam, dirs, p); %Trials with masking or no... 
 
-[expParam, p]      = dfSetAudFB(expParam, dirs, p); %Trials with masking or no... 
+    expParam.trialType = dfSetTrialOrder(expParam.numTrial, expParam.perCatch); %numTrials, percentCatch
 
-expParam.trialType = dfSetTrialOrder(expParam.numTrial, expParam.perCatch); %numTrials, percentCatch
+    [expParam.sigs, expParam.trigs] = dfMakePertSignal(expParam.trialLen, expParam.numTrial, expParam.sRateQ, expParam.sRateAnal, expParam.trialType, expParam.expType);
 
-[expParam.sigs, expParam.trigs] = dfMakePertSignal(expParam.trialLen, expParam.numTrial, expParam.sRateQ, expParam.sRateAnal, expParam.trialType, expParam.expType);
+    %Create a negative voltage signal for the force sensors
+    negVolSrc = zeros(expParam.sRateQ*expParam.trialLen, 1) - 1;
+    negVolSrc(1) = 0; negVolSrc(end) = 0;
 
-%Create a negative voltage signal for the force sensors
-negVolSrc = zeros(expParam.sRateQ*expParam.trialLen, 1) - 1;
-negVolSrc(1) = 0; negVolSrc(end) = 0;
+    expParam.resPause = 2.0;
 
-expParam.resPause = 2.0;
+    %Taking the first trial for ease. File out will be 'data'
+    d = dir([dirs.SavFileDir, '\*.mat']);
+    fnames = sort_nat({d.name}); 
+    load(fullfile(dirs.SavFileDir, fnames{expParam.offLineTrial})); 
 
-%Taking the first trial for ease. File out will be 'data'
-d = dir([dirs.SavFileDir, '\*.mat']);
-fnames = sort_nat({d.name}); 
-load(fullfile(dirs.SavFileDir, fnames{expParam.offLineTrial})); 
+    Mraw  = data.signalIn; 
+    fs    = data.params.sRate;
 
-Mraw  = data.signalIn; 
-fs    = data.params.sRate;
+    DAQin   = [];
+    rawData = [];
+    for ii = 1:expParam.numTrial
+        expParam.curTrial   = ['Trial' num2str(ii)];
+        expParam.curSubCond = [expParam.subject expParam.run expParam.curTrial];
 
-for ii = 1:expParam.numTrial
-    expParam.curTrial   = ['Trial' num2str(ii)];
-    expParam.curSubCond = [expParam.subject expParam.run expParam.curTrial];
-    
-    audStimP = dfSetAudapFiles(InflaRespRoute, tStep, expParam.ostFN, expParam.pcfFN, expParam.trialType(ii), expParam.trigs(ii,:,1), expParam.stimType);
-    
-    %Set the OST and PCF functions
-    Audapter('ost', expParam.ostFN, 0);
-    Audapter('pcf', expParam.pcfFN, 0);
-    
-    %Resample at 48000Hz
-    Mraw_reSamp = resample(Mraw, data.params.sr * data.params.downFact, fs);
-    %Split the signal into frames
-    Mraw_frames = makecell(Mraw_reSamp, data.params.frameLen * data.params.downFact);
-    
-    fprintf('Trial %d\n', ii)
-    AudapterIO('init', p);
-    Audapter('reset');
+        audStimP = dfSetAudapFiles(InflaRespRoute, tStep, expParam.ostFN, expParam.pcfFN, expParam.trialType(ii), expParam.trigs(ii,:,1), expParam.stimType);
 
-    for n = 1:length(Mraw_frames)
-        Audapter('runFrame', Mraw_frames{n});
+        %Set the OST and PCF functions
+        Audapter('ost', expParam.ostFN, 0);
+        Audapter('pcf', expParam.pcfFN, 0);
+
+        %Resample at 48000Hz
+        Mraw_reSamp = resample(Mraw, data.params.sr * data.params.downFact, fs);
+        %Split the signal into frames
+        Mraw_frames = makecell(Mraw_reSamp, data.params.frameLen * data.params.downFact);
+
+        fprintf('Trial %d\n', ii)
+        AudapterIO('init', p);
+        Audapter('reset');
+
+        for n = 1:length(Mraw_frames)
+            Audapter('runFrame', Mraw_frames{n});
+        end
+
+        NIDAQsig = [expParam.sigs(:,ii) negVolSrc];
+        queueOutputData(s, NIDAQsig);
+
+        [dataDAQ, time] = s.startForeground;
+
+        data = dfSaveRawData(expParam, dirs);
+        DAQin = cat(3, DAQin, dataDAQ);
+        rawData = cat(1, rawData, data);
+        
+        if expParam.bPlay; soundsc(data_off.signalIn, data_off.expParam.sRateAnal); end
+
+        pause(expParam.resPause)
     end
     
-    NIDAQsig = [expParam.sigs(:,ii) negVolSrc];
-    queueOutputData(s, NIDAQsig);
-    
-    [dataDAQ, time] = s.startForeground;
-    
-    data_off = svData(expParam, dirs, p, audStimP, dataDAQ);
+    OA.dirs        = dirs;
+    OA.expParam    = expParam;
+    OA.p           = p;
+    OA.audStimP    = audStimP;
+    OA.DAQin       = DAQin;
+    OA.rawData     = rawData;      
 
-    if expParam.bPlay; soundsc(data_off.signalIn, data_off.expParam.sRateAnal); end
-    
-    pause(expParam.resPause)
-end
-end
-
-function data = svData(expParam, dirs, p, audStimP, dataDAQ)
-%Package all the data into something that is useful for analysis
-
-try
-    data = AudapterIO('getData');
-    
-    data.expParam    = expParam; %Experimental Parameters
-    data.dirs        = dirs;     %Directories
-    data.p           = p;        %Audapter Parameters
-    data.audStimP    = audStimP; %auditory stimulus Parameters
-    data.DAQin       = dataDAQ;  %NIDAQ recordings ('Force Sensors')
-    save(fullfile(dirs.RecFileDir, [expParam.curSubCond dirs.saveFileSuffix]), 'data')
-
-    audiowrite(fullfile(dirs.RecWaveDir,[expParam.curSubCond dirs.saveFileSuffix '_headOut.wav']), data.signalOut, expParam.sRateAnal)
-    audiowrite(fullfile(dirs.RecWaveDir,[expParam.curSubCond dirs.saveFileSuffix '_micIn.wav']), data.signalIn, expParam.sRateAnal)
-catch
-    disp('Audapter decided not to show up today')
-    data = [];
-    return
+    dirs.RecFileDir = fullfile(dirs.RecFileDir, [expParam.subject '_OfflineAud.mat']);
+    save(dirs.RecFileDir, 'OA')
+else
 end
 end
 
