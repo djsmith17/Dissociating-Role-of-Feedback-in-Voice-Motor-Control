06a3283737e227bb2176e64e24a3610b3e836938
Dante Smith <djsmith@bu.edu>
2017-08-29 18:16:01 -0400

    fixing some indentation so I can read it.
    following the logic, and making my own script.

--- Praatbatch/praamatlab.m
+++ Praatbatch/praamatlab.m
@@ -1,49 +1,51 @@
    
 newmic = [micFolder, '\']; %add a slash to the mic folder
-    ext = '.wav'; %extension of files
+ext = '.wav'; %extension of files
 
 
 %% make sure you have the scripts you need for praat interface to work
-    pi_dir = 'praat_interface' ;
-    if ~exist( pi_dir , 'dir' )
-        error( 'folder ''praat_interface'' not found in directory' )
-    end
-    p_fn = fullfile( pi_dir , 'praat.exe' ) ;
-    if ~exist( p_fn , 'file' )
-        error( 'file ''praat.exe'' not found' )
-    end
-    sp_fn = fullfile( pi_dir , 'sendpraat.exe' ) ;
-    if ~exist( sp_fn , 'file' )
-        error( 'file ''sendpraat.exe'' not found' )
-    end
-    gp_fn = fullfile( pi_dir , 'get_f0_EET.praat' ) ;
-    if ~exist( gp_fn , 'file' )
-        error( 'file ''get_f0_EET.praat'' not found' )
-    end
+pi_dir = 'praat_interface' ;
+if ~exist(pi_dir , 'dir')
+    error('folder ''praat_interface'' not found in directory')
+end
+
+p_fn = fullfile(pi_dir , 'praat.exe') ;
+if ~exist(p_fn , 'file' )
+    error('file ''praat.exe'' not found')
+end
+
+sp_fn = fullfile(pi_dir , 'sendpraat.exe') ;
+if ~exist( sp_fn , 'file' )
+    error( 'file ''sendpraat.exe'' not found' )
+end
+gp_fn = fullfile( pi_dir , 'get_f0_EET.praat' ) ;
+if ~exist( gp_fn , 'file' )
+    error( 'file ''get_f0_EET.praat'' not found' )
+end
     
 %% uncomment to run praat script
-    %Build DOS calls to control praat
-    call2 = sprintf( '%s praat "execute %s %s %s %s %f %f %f %f %f"' , ...
-        fullfile( pwd , sp_fn ) , ... %location of sendpraat.exe
-        fullfile( pwd , gp_fn ) , ... %location of praat script wrote (get_fo_EET.praat)
-        newmic , ... %where wav files are located aka where you ONE new file is made
-        ext , ... %extension of files 
-        txtmicFolder, ... %destination where .txt files will stay % where you want your output to save (wav file)
-        voicingThreshold,  ... %voicing threshold indicated %probably won't need
-        pitchFloor, ... %pitch floor indicated %might want to set
-        pitchCeiling, ... %pitch ceiling indicated %might want to set
-        maxformant, ... %max formant value in % won't need
-        numformant ... %number of formants to calculate %won't need
-        ) ;
+%Build DOS calls to control praat
+call2 = sprintf( '%s praat "execute %s %s %s %s %f %f %f %f %f"' , ...
+    fullfile( pwd , sp_fn ) , ... %location of sendpraat.exe
+    fullfile( pwd , gp_fn ) , ... %location of praat script wrote (get_fo_EET.praat)
+    newmic , ... %where wav files are located aka where you ONE new file is made
+    ext , ... %extension of files
+    txtmicFolder, ... %destination where .txt files will stay % where you want your output to save (wav file)
+    voicingThreshold,  ... %voicing threshold indicated %probably won't need
+    pitchFloor, ... %pitch floor indicated %might want to set
+    pitchCeiling, ... %pitch ceiling indicated %might want to set
+    maxformant, ... %max formant value in % won't need
+    numformant ... %number of formants to calculate %won't need
+    ) ;
     
     
     
+[s, r] = dos(call2);
+if s ~= 0
+    dos( [ fullfile( pwd , p_fn ) ' &' ] ) ;
     [ s , r ] = dos( call2 ) ;
     if s ~= 0
-        dos( [ fullfile( pwd , p_fn ) ' &' ] ) ;
-        [ s , r ] = dos( call2 ) ;
-        if s ~= 0
-            disp( r )
-            error( 'ERROR: something went wrong' )
-        end
+        disp(r)
+        error('ERROR: something went wrong')
     end
+end
