917b7bbf6016310cb40e547d569a0e73a7b838ee
Dante Smith <djsmith@bu.edu>
2017-08-30 16:37:04 -0400

    some more renaming
    I am going to now make some bigger changes to batchf0JNDTokens.praat
    that should reflect how MATLAB will handle it. I wanted to do one commit
    before making some potentiall breaking changes

--- Ayoub_VoiceJND/generatef0JNDTokens.m
+++ Ayoub_VoiceJND/generatef0JNDTokens.m
@@ -4,7 +4,7 @@ tokenDir = dirs.tokenDir;
 psDir    = fullfile(dirs.code, presentation);        %Praat scripting
 pbDir    = fullfile(dirs.helpers, 'praat batching'); %Praat batching
 
-newmic = [micFolder, '\']; %add a slash to the mic folder
+tokenDir = [tokenDir, '\']; %add a slash to the mic folder
 ext = '.wav'; %extension of files
 
 p_fn = fullfile(pbDir, 'praat.exe');
@@ -13,27 +13,24 @@ if ~exist(p_fn, 'file')
 end
 
 sp_fn = fullfile(pbDir, 'sendpraat.exe');
-if ~exist( sp_fn , 'file' )
-    error( 'file ''sendpraat.exe'' not found')
+if ~exist(sp_fn, 'file')
+    error('file ''sendpraat.exe'' not found')
 end
 
-gp_fn = fullfile(psDir, 'generatef0JNDTokens');
-if ~exist(gp_fn, 'file')
-    error('file ''generatef0JNDTokens'' not found')
+gt_fn = fullfile(psDir, 'batchf0JNDTokens.praat');
+if ~exist(gt_fn, 'file')
+    error('file ''batchf0JNDTokens.praat'' not found')
 end
     
 %Build DOS calls to control praat
-call2 = sprintf('%s praat "execute %s %s %s %s %f %f %f %f %f"', ...
-                sp_fn, ... %location of sendpraat.exe
-                gp_fn, ... %location of praat script wrote (get_fo_EET.praat)
-                newmic, ... %where wav files are located aka where you ONE new file is made
-                ext, ... %extension of files
+call2 = sprintf('%s praat "execute %s %s %s %s %f %f"', ...
+                sp_fn, ... %sendpraat.exe
+                gt_fn, ... %saved praat script ('generatef0JNDTokens)
+                tokenDir, ... %file location for saved wav files
+                ext, ... %file extension
                 txtmicFolder, ... %destination where .txt files will stay % where you want your output to save (wav file)
-                voicingThreshold,  ... %voicing threshold indicated %probably won't need
                 pitchFloor, ... %pitch floor indicated %might want to set
                 pitchCeiling, ... %pitch ceiling indicated %might want to set
-                maxformant, ... %max formant value in % won't need
-                numformant ... %number of formants to calculate %won't need
                 );    
     
 [s, r] = dos(call2);
