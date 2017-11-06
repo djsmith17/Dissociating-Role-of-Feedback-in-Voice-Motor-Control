499a24a6736d2cf13ea49e17459d357b032d3af4
Dante Smith <djsmith@bu.edu>
2017-11-06 04:16:01 -0500

    pooled analysis data file
    its going to be lit. I promise

--- Analysis/dfRunPooledAnalysis.m
+++ Analysis/dfRunPooledAnalysis.m
@@ -7,13 +7,42 @@ function dfRunPooledAnalysis()
 %Require the Signal Processing Toolbox
 
 clear all; close all; clc
-pA.project       = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control';
+pA.project       = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control'; 
+pA.pAnalysis     = 'SfN2017';
 pA.participants  = {'Pilot24'; 'Pilot24'}; %List of multiple participants.
-pA.runs          = {'SF1'; 'SF2'};
+pA.numPart       = length(pA.participants);
+pA.runs          = {'SF1'; 'SF2'}; %All runs to consider 
+pA.numRuns       = length(pA.runs);
+
+dirs               = dfDirs(pA.project);
+dirs.SavResultsDir = fullfile(dirs.Results, 'Pooled Analyses', pA.pAnalysis);
+
+if exist(dirs.SavResultsDir, 'dir') == 0
+    mkdir(dirs.SavResultsDir)
+end
+
+for ii = 1:pA.numPart
+    for jj = 1:pA.numRuns
+        participant = pA.participants{ii};
+        run         = pA.runs{jj};
+        dirs.SavFileDir  = fullfile(dirs.Results, participant, run); %Where to save results        
+        dirs.SavFile     = fullfile(dirs.SavFileDir, [participant run 'ResultsDRF.mat']);
+        
+        if exist(dirs.SavFile, 'file') == 0
+            disp('ERROR: NO DANG FILE')
+            return
+        end        
+        load(dirs.SavFile)
+       
+        niPAn  = [];
+        niPRes = [];
+        auPAn  = [];
+        auPRes = [];
+    end
+end
 
 
-
-
-
-
+dirs.SavResultsFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'ResultsDRF.mat']);
+fprintf('Saving Pooled Analysis for %s\n', pA.pAnalysis)
+save(dirs.SavResultsFile, 'auPAn', 'auPRes', 'niPAn', 'niPRes')
 end
