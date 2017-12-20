ce57e44ba9058ede52e32441fe389c17db069305
Dante Smith <djsmith@bu.edu>
2017-12-20 00:36:24 -0500

    making sure I have the baseline f0 recording

--- Analysis/OfflineAudapterPlay.m
+++ Analysis/OfflineAudapterPlay.m
@@ -188,6 +188,15 @@ else
 end
 close all
 
+GTFile = [expParam.subject GT1 'DRF.mat'];
+dirs.GTBaseFile = fullfile(dirs.SavData, expParam.subject, 'GT1', GTFile);
+if ~exist(dirs.SavBaseFile, 'file')
+    fprintf('ERROR: No GT file at %s!\n', dirs.GTBasFile)
+    return
+end
+load(dirs.GTBaseFile)
+
+bTf0b = GT.subjf0;
 [auAn, auRes] = dfAnalysisAudapter(dirs, OA.expParam, OA.rawData, bTf0b, 1);
 
 drawDAQMeanTrialMicf0(auRes, dirs.SavResultsDir)
