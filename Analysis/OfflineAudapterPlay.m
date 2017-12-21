5a066d4a8a9fc65b25c02e6b81bff3bf0c3b5f14
Dante Smith <djsmith@bu.edu>
2017-12-21 03:34:01 -0500

    another version with a longer pert

--- Analysis/OfflineAudapterPlay.m
+++ Analysis/OfflineAudapterPlay.m
@@ -38,7 +38,7 @@ switch pertType
         pertTypeSw = 1;
 end
 
-collectNewData         = 0; %Boolean
+collectNewData         = 1; %Boolean
 
 %Experiment Configurations
 expParam.project      = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control';
