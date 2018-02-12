2e069d90e5aff8ac2b4208d9c2ec66147a0972df
Dante Smith <djsmith@bu.edu>
2018-02-12 10:39:32 -0500

    fixing Trial 1 error

--- Presentation/dfUpdateVisFB.m
+++ Presentation/dfUpdateVisFB.m
@@ -4,6 +4,11 @@ function [color, newPos] = dfUpdateVisFB(anMsr, rms)
 %Convert rms SPL to dB and take the mean
 refSPL  = 0.00002;      %20 micropascals for air
 rmsdB   = 20*log10(rms/refSPL);
+
+%There were -Inf in my RMSdB. not exactly sure why, but this fixes the
+%trial 1, lack of feeedback
+rmsdB(find(rmsdB == -Inf)) = 0;
+
 meanAmp = mean(rmsdB);
 
 %New Height
