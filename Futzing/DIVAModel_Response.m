e5ea620432bd8e0924a6cb197dba1bcba802624c
Dante Smith <djsmith@bu.edu>
2018-03-05 05:47:51 -0500

    Turns out...I want it in this branch
    I will need to reconcile a better way to do this in the future..but for now. I do it this way

---
+++ Futzing/DIVAModel_Response.m
@@ -0,0 +1,47 @@
+
+close all; clear all;
+
+figDir = 'C:\Users\djsmith\OneDrive\Dissertation\Media\Diagrams';
+
+%Plotting Variables
+plotpos    = [100 100];
+plotdim    = [600 800];
+DIVAResp   = figure('Color', [1 1 1]);
+set(DIVAResp, 'Position',[plotpos plotdim],'PaperPositionMode','auto')
+
+fontN        = 'Arial';
+legAnnoFSize = 25;
+titleFSize   = 20;
+axisLSize    = 30;
+lineThick    = 4;
+
+fs = 10000; %samp/s
+t = 0:1/fs:4-1/fs;
+
+tOnset  = 1*fs;
+tOffset = 3*fs;
+
+tPitchRespOnset = (0:.50*fs) + tOnset;
+lenPitchResp = length(tPitchRespOnset);
+
+fpitch = 1/(2*lenPitchResp);
+
+pertSig = zeros(size(t));
+pertSig(tOnset:tOffset) = 1;
+pertSig = pertSig + 3;
+
+pitchSig = zeros(size(t));
+pitchSig(tPitchRespOnset) = -sin(2*pi*fpitch*(1:lenPitchResp));
+pitchSig = pitchSig - 2;
+
+plot(t, pertSig, 'k', 'LineWidth', lineThick)
+hold on
+plot(t, pitchSig, 'r', 'LineWidth', lineThick)
+
+title('Laryngeal Displacement Dynamics', 'FontName', fontN, 'FontSize', titleFSize, 'FontWeight', 'bold')
+axis([0 4 -5 5]); box off;
+% axis off
+
+fileName = 'LDD Dynamics.jpg';
+figFile = fullfile(figDir, fileName);
+export_fig(figFile)
---
+++ Futzing/testNIDAQTriggers.m
@@ -0,0 +1,57 @@
+function testNIDAQTriggers()
+
+% This function uses:
+% -dfSetTrialOrder
+% -dfMakePertSignal
+% -dfSetVisFB
+% -initNIDAQSess
+
+close all; clc
+expParam.expType  = 'Somatosensory Perturbation_Perceptual';
+expParam.dev      = 'Dev2';
+expParam.numTrial = 3;
+expParam.perCatch = 1;
+expParam.trialLen = 4;
+expParam.sRateQ   = 8000;
+expParam.sRateAnal= 16000;
+expParam.resPause = 3;
+
+% recordedRMS = (5+rand(1991,1))/1000;
+
+targRMS   = 50; % dB just to test
+boundsRMS = 3;  % +/- dB
+
+expParam.trialType              = dfSetTrialOrder(expParam.numTrial, expParam.perCatch);
+[expParam.sigs, expParam.trigs] = dfMakePertSignal(expParam.trialLen, expParam.numTrial, expParam.sRateQ, expParam.sRateAnal, expParam.trialType, expParam.expType, 1);
+
+[anMsr, H1, H2, H3, fbLines, rec, trigCirc] = dfSetVisFB(targRMS, boundsRMS);
+
+%Initialize NIDAQ
+[s, sd, niCh]  = initNIDAQSess(expParam.dev, expParam.numTrial, expParam.trialLen, expParam.sigs);
+
+backgroundVS(expParam, s, sd, H2, H3, trigCirc)
+end
+
+function backgroundVS(expParam, s, sd, H2, H3, trigCirc)
+
+queuePertOutputData(sd, s)
+startBackground(s);
+pause(4.0)
+set(H3,'Visible','off');
+
+for ii = 1:expParam.numTrial
+    fprintf('Running Trial %d\n', ii)
+    set([H2 trigCirc],'Visible','on');
+    pause(expParam.trialLen)
+    set([H2 trigCirc],'Visible','off');
+    pause(expParam.resPause)
+end
+close all
+stop(s)
+
+time = sd.recTime;
+data = sd.recData;
+
+figure
+plot(time, data)
+end
