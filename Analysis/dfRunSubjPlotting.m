fb3ea6fadea824d92beb371703346849ff8643da
Smith <djsmith@bu.edu>
2018-03-12 01:01:16 -0400

    fixed plots
    so things are cut off

--- Analysis/dfRunSubjPlotting.m
+++ Analysis/dfRunSubjPlotting.m
@@ -11,9 +11,9 @@ function dfRunSubjPlotting()
 
 close all;
 sPlt.project       = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control';
-sPlt.participants  = {'Pilot28'}; %List of multiple participants.
+sPlt.participants  = {'Pilot0'}; %List of multiple participants.
 sPlt.numPart       = length(sPlt.participants);
-sPlt.runs          = {'DP1_1'}; %All runs to consider 
+sPlt.runs          = {'DP3_1'}; %All runs to consider 
 sPlt.numRuns       = length(sPlt.runs);
 dirs               = dfDirs(sPlt.project);
 
