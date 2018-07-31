e2da636f3607c86d0c64cc3fa3f7ad9f2d459b13
Dante Smith <djsmith@bu.edu>
2018-07-31 17:29:31 -0400

    fixing this syntax
    the syntax was wrong

--- Presentation/dfRunSFPerturb.m
+++ Presentation/dfRunSFPerturb.m
@@ -27,7 +27,7 @@ ET = tic;
 rng('shuffle');
 
 % Main Experimental prompt: Subject/Run Information
-subject    = 'null';   % Subject#, Pilot#, null
+subject    = 'DRF_MN1';   % Subject#, Pilot#, null
 run        = prompt4RunName();
 
 balloon    = '2E4';     % Which perturbation balloon?
