ece94bfe166d46b2cf824d9c6f1a5dc213c16b20
Dante Smith <djsmith@bu.edu>
2018-07-31 11:27:05 -0400

    reopening this function for testing wav files
    taking a quick gander at the feasability of running masking noise from MATLAB, instead of from desktop

--- Presentation/dfPlayMaskingNoise.m
+++ Presentation/dfPlayMaskingNoise.m
@@ -13,7 +13,7 @@ function dfPlayMaskingNoise()
 expParam.project    = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control';
 expParam.expType    = 'Somatosensory Perturbation_Perceptual';
 expParam.gender     = 'male';
-expParam.trialLen   = 100;  % Seconds
+expParam.trialLen   = 4;  % Seconds
 expParam.numTrial   = 1;
 expParam.AudFBSw    = 2;    % Masking Noise
 
@@ -42,21 +42,53 @@ expParam.pcfFN = fullfile(dirs.Prelim, 'SFPerturbPCF.pcf'); check_file(expParam.
 % Set up Auditory Feedback (Masking Noise)
 [expParam, p]      = dfSetAudFB(expParam, dirs, p);
 
+% Pause Periods
+expParam.readyPause = 5.0;
+expParam.cuePause  = 1.0; % How long the cue period lasts
+expParam.buffPause = 0.8; %Give them a moment to start speaking
+expParam.endPause  = 0.5;
+expParam.resPause  = 2.0; % How long the rest/VisFB lasts
+
+noiseLen = calcMaskLen(expParam);
+
+ET = tic;
+%Open the curtains
+pause(expParam.readyPause);                % Let them breathe a sec
+
 for ii = 1:expParam.numTrial
     
-    fprintf('Ready to listen?\n')
-    pause()
     %Set the OST and PCF functions
     Audapter('ost', expParam.ostFN, 0);
     Audapter('pcf', expParam.pcfFN, 0);
+    
+    pause(expParam.cuePause)
     
     fprintf('Trial %d\n', ii)
     AudapterIO('init', p);
     Audapter('reset');
     Audapter('start');
+    pause(expParam.buffPause)
 
-    pause(expParam.trialLen);
+    pause(expParam.trialLen)
+    pause(expParam.endPause)
     
-    Audapter('stop');   
+    Audapter('stop');
+    pause(expParam.resPause)
 end
+elapsed_time = toc(ET);   % Elapsed Time of the experiment
+fprintf('\nElapsed Time: %f (s)\n', elapsed_time)
+end
+
+function noiseLen = calcMaskLen(expParam)
+
+numTrial = expParam.numTrial;
+
+rdyTime = expParam.readyPause;
+cueTime = expParam.cuePause;
+buffTime = expParam.buffPause;
+trlTime  = expParam.trialLen;
+endTime  = expParam.endPause;
+resTime  = expParam.resPause;
+
+noiseLen = rdyTime + (cueTime + buffTime + trlTime + endTime + resTime)*numTrial;
 end
