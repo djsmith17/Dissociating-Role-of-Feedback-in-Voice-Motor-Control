52aa52a02a8d5a7bb86527efd7cdfac12f7183e2
Dante Smith <djsmith@bu.edu>
2018-08-07 15:27:53 -0400

    updating the SetAudFB to always have correct loudness
    I was hard setting the headphone gain, but that was lazy. Now the dB value is set in the individual experiments, and the gain decimal value is calculated based on the input value. I am also setting up the setAudFB to be more congrous with the new version of Audapter, so that the other branches can take advantage of these changes.

--- Presentation/dfSetAudFB.m
+++ Presentation/dfSetAudFB.m
@@ -10,33 +10,43 @@ function [expParam, p] = dfSetAudFB(expParam, dirs, p)
 % NORMAL AUDITORY FEEDBACK OF THEIR VOICE
 % PITCH-SHIFTED AUDITORY FEEDBACK OF THEIR VOICE
 % SPEECH-SHAPED MASKING NOISE
+%
+% This function has a subfunction below named: setLoudRatio
+  
+dB           = expParam.headGain;
+gender       = expParam.gender;
+p.bBypassFmt = 1;               % No Formant tracking
+p.dScale     = setLoudRatio(dB);% Scale of output from input
+p.nDelay     = 7;
+
+if isequal(lower(gender), 'female')
+    p.pitchLowerBoundHz = 150;
+    p.pitchUpperBoundHz = 300;
+elseif isequal(lower(gender), 'male')
+    p.pitchLowerBoundHz = 80;
+    p.pitchUpperBoundHz = 160;
+end 
+
+expParam.SSNw   = [];
+expParam.SSNfs  = [];
 
 %NORMAL AUDITORY FEEDBACK OF THEIR VOICE
 if  expParam.AudFBSw == 0
-    p.fb          = 1;  % Microphone FB
-    p.bPitchShift = 0;  % No pitch-shifting
-    p.bBypassFmt  = 1;  % No Formant tracking
-    p.dScale      = 1.78;   %5dB
-    
-    expParam.SSNw   = [];
-    expParam.SSNfs  = [];
+    p.fb                = 1; % Microphone FB
+    p.bTimeDomainShift  = 0; % No pitch-shifting
+    p.bCepsLift         = 0;
     
 %PITCH-SHIFTED AUDITORY FEEDBACK OF THEIR VOICE
 elseif expParam.AudFBSw == 1
-    p.fb          = 1;    % Microphone FB
-    p.bPitchShift = 1;    % Pitch-shifting
-    p.bBypassFmt  = 1;    % No Formant tracking
-    p.dScale      = 1.78; %Headphone Scalar 5dB
-    
-    expParam.SSNw   = [];
-    expParam.SSNfs  = [];
+    p.fb                = 1; % Microphone FB
+    p.bTimeDomainShift  = 1; % Pitch-shifting
+    p.bCepsLift         = 1;
 
 %SPEECH-SHAPED MASKING NOISE
 elseif expParam.AudFBSw == 2
-    p.fb          = 0;    % Audio File (Masking)
-    p.bPitchShift = 0;
-    p.bBypassFmt  = 1;    % No Formant tracking
-    p.dScale      = 1; % Headphone Scalar
+    p.fb                = 0; % Audio File (Masking)
+    p.bTimeDomainShift  = 0; % No pitch-shifting
+    p.bCepsLift         = 0;
     
     %Uses Speech-Shaped Noise stored in util
     noiseWavFN = fullfile(dirs.Prelim, 'SSN.wav'); 
@@ -60,4 +70,12 @@ else
     disp('ERROR in dfSetAudFB: Inappropriate feedback method selected')
     return
 end 
+end
+
+function dScale = setLoudRatio(dB)
+% dB should be a positive or negative decimal value of change in dB
+% representing how much you want to scale the headphones against the input 
+% microphone level.
+
+dScale = 10^(dB/20);
 end
