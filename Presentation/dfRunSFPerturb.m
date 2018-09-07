66521ee021e98b2bdce5a0e46610d306bfb1e203
Dante Smith <djsmith@bu.edu>
2018-09-07 16:26:52 -0400

    updating these
    so that they dont break later

--- Presentation/dfRunSFPerturb.m
+++ Presentation/dfRunSFPerturb.m
@@ -27,7 +27,7 @@ ET = tic;
 rng('shuffle');
 
 % Main Experimental prompt: Subject/Run Information
-subject    = 'DRF_MN19';   % Subject#, Pilot#, null
+subject    = 'DRF_MN20';   % Subject#, Pilot#, null
 run        = prompt4RunName();
 
 balloon    = '2E4';     % Which perturbation balloon?
@@ -149,11 +149,11 @@ expParam.boundsRMS = 3;   % +/- dB
 fprintf('\nStarting Trials\n\n')
 
 % Dim the lights (Set the visual Feedback)
-[anMsr, H1, H2, H3, fbLines, rec, trigCirc] = dfSetVisFB(expParam.curSess, expParam.targRMS, expParam.boundsRMS);
+[msrStr, annoStr] = dfSetVisFB(expParam.curSess, expParam.targRMS, expParam.boundsRMS);
 
 %Open the curtains
 pause(5);                % Let them breathe a sec
-set(H3,'Visible','off'); % Turn off 'Ready?'
+set(annoStr.Ready, 'Visible','off'); % Turn off 'Ready?'
 
 DAQin = []; rawData = [];
 loudResults = [];
@@ -175,12 +175,12 @@ for ii = 1:expParam.numTrial
     queueOutputData(s, NIDAQsig);
     
     %Cue to begin trial
-    set(H1,'Visible','on');
+    set(annoStr.plus, 'Visible','on');
     pause(expParam.cuePause)
     
     %Phonation Start
-    set(H1,'Visible','off');
-    set([H2 trigCirc],'Visible','on');
+    set(annoStr.plus, 'Visible','off');
+    set([annoStr.EEE annoStr.visTrig],'Visible','on');
     
     fprintf('Trial %d\n',ii)
     AudapterIO('init', p);
@@ -193,7 +193,7 @@ for ii = 1:expParam.numTrial
     [dataDAQ, ~] = s.startForeground;
      
     %Phonation End
-    set([H2 trigCirc],'Visible','off');
+    set([annoStr.EEE annoStr.visTrig],'Visible','off');
     pause(expParam.endPause)
     Audapter('stop');
     
@@ -205,19 +205,19 @@ for ii = 1:expParam.numTrial
     %Grab smooth RMS trace from 'data' structure
     rmsMean = dfCalcMeanRMS(data, expParam.rmsB);
     %Compare against baseline and updated Visual Feedback
-    [color, newPos, loudResult] = dfUpdateVisFB(anMsr, rmsMean);
+    [color, newPos, loudResult] = dfUpdateVisFB(msrStr, rmsMean);
     loudResults = cat(1, loudResults, loudResult);
     dispLoudnessResult(loudResult)
 
-    set(rec, 'position', newPos);
-    set(rec, 'Color', color); set(rec, 'FaceColor', color);
-    set([rec fbLines], 'Visible', 'on');
+    set(annoStr.LoudRec, 'position', newPos);
+    set(annoStr.LoudRec, 'Color', color, 'FaceColor', color);
+    set([annoStr.LoudRec annoStr.fbLines], 'Visible', 'on');
     
     LR = LR.updateLiveResult(dataDAQ, ii);
     
     dfSaveWavRec(data, expParam, dirs);    
     pause(expParam.resPause)
-    set([rec fbLines], 'Visible', 'off');
+    set([annoStr.LoudRec annoStr.fbLines], 'Visible', 'off');
 end
 close all;
 elapsed_time = toc(ET)/60;   % Elapsed Time of the experiment
