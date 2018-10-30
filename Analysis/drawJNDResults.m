59faed84f63502745b8ca54d95f4631c3660ce48
Dante Smith <djsmith@bu.edu>
2018-10-30 17:39:26 -0400

    Updated JND Plot
    Removing some of the stats such as num trials, num reversals..etc. cleaner and larger text. Easier to read on a slide

--- Analysis/drawJNDResults.m
+++ Analysis/drawJNDResults.m
@@ -21,10 +21,10 @@ tColors = [[0 0 1];      % Correct Different
            [0.5 0.5 1];  % Correct Same
            [1 0.5 0.5]]; % Incorrect Same
 
-annoPos = [.38 .79;
-           .87 .79;
-           .38 .32;
-           .87 .32]; 
+annoPos = [.32 .785;
+           .81 .785;
+           .32 .315;
+           .81 .315]; 
        
 aH = [0 0 0 0 0];
 
@@ -35,17 +35,18 @@ ha = tight_subplot(2, 2, [0.15 0.06], [0.1 0.1], [0.05 0.03]);
 
 for ii = 1:numRuns
     UD        = allRunData(ii);
-    meanJND   = JNDScores(ii);
+    JNDScore  = JNDScores(ii);
     catchAccu = lastSetAcc(ii);
-    revNote   = [num2str(UD.reversals) ' Reversals, '];
-    triNote   = [num2str(UD.performedTrials) '/' num2str(UD.totalTrials) ' Trials, '];
-    timNote   = [num2str(round(10*UD.elapsedTime)/10) ' min, '];
+%     revNote   = [num2str(UD.reversals) ' Reversals, '];
+%     triNote   = [num2str(UD.performedTrials) '/' num2str(UD.totalTrials) ' Trials, '];
+%     timNote   = [num2str(round(10*UD.elapsedTime)/10) ' min, '];
+%     
+%     if isfield(UD, 'inst')
+%        timNote   = [timNote 'Instru: ' UD.inst];
+%     end
     
-    if isfield(UD, 'inst')
-       timNote   = [timNote 'Instru: ' UD.inst];
-    end
-    
-    anno2 = ['Catch Accuracy: ' num2str(catchAccu) '%'];
+    scoreNote = ['JND Score: ' num2str(JNDScore) ' cents'];
+    accurNote = ['Last Trials Accuracy: ' num2str(catchAccu) '%'];
     
     axes(ha(ii))
     rV = plot(find(UD.reversal~=0), UD.x(find(UD.reversal~=0)),'o','MarkerFaceColor',[1 1 1],'MarkerEdgeColor',[0 0 0],'MarkerSize',12);
@@ -57,18 +58,18 @@ for ii = 1:numRuns
     cS = plot(find(UD.allTrialTypes==3), UD.x(find(UD.allTrialTypes==3)),'o','MarkerFaceColor',tColors(3,:),'MarkerEdgeColor',tColors(3,:),'MarkerSize',10);
     iS = plot(find(UD.allTrialTypes==4), UD.x(find(UD.allTrialTypes==4)),'o','MarkerFaceColor',tColors(4,:),'MarkerEdgeColor',tColors(4,:),'MarkerSize',10);
        
-    aJ = line([0 length(UD.response)], [meanJND meanJND],'LineStyle', '-.', 'LineWidth',3,'color',[1 0 1]);
-    title(['f0 Acuity JND ' num2str(ii) ': ' revNote triNote timNote], 'FontSize', titleFS, 'FontName', 'Arial', 'FontWeight', 'bold')
+    aJ = line([0 length(UD.response)], [JNDScore JNDScore],'LineStyle', '-.', 'LineWidth',3,'color',[1 0 1]);
+%     title(['f0 Acuity JND ' num2str(ii) ': ' revNote triNote timNote], 'FontSize', titleFS, 'FontName', 'Arial', 'FontWeight', 'bold')
+    title(runs{ii}, 'FontSize', titleFS, 'FontName', 'Arial', 'FontWeight', 'bold')
     xlabel('Trials','FontSize', xyFS,'FontName','Arial', 'FontWeight', 'bold');
     ylabel('f0 Distance (cents)','FontSize', xyFS,'FontName','Arial', 'FontWeight', 'bold');
     axis([0 80 0 80])
     
     t = annotation('textbox',[annoPos(ii,1) annoPos(ii,2) 0.45 0.1],...
-                   'string', {['Mean JND Score: ' num2str(meanJND) ' cents'];...
-                                                  anno2},...
+                   'string', {scoreNote; accurNote},...
                     'LineStyle','none',...
                     'FontWeight','bold',...
-                    'FontSize',8,...
+                    'FontSize',14,...
                     'FontName','Arial');
     
     set(gca, 'LineWidth', 2,...
@@ -97,9 +98,9 @@ suptitle({'f0 Acuity JND', [JNDa.participant ', f0 = ' num2str(JNDa.f0) ' Hz'], 
 
 legend(aH,{['Correct ' JNDa.tN{1}],['Incorrect ' JNDa.tN{1}],'Reversals',['Correct ' JNDa.tN{2}],['Incorrect ' JNDa.tN{2}]},...
           'Orientation','Horizontal',...
-          'FontSize', 8,...
-          'Position', [0.51 0.48 0.01 0.01],...
-          'EdgeColor', [0 0 0])
+          'FontSize', 12,...
+          'Position', [0.51 0.455 0.02 0.05],...
+          'EdgeColor', [0.5 0.5 0.5])
 
 
 plTitle = [JNDa.participant 'JNDStaircaseResults' JNDa.runType '.jpg'];     
