f514e462892cae1dba02e86b3cb4441dbef645fc
Dante Smith <djsmith@bu.edu>
2018-11-02 16:41:36 -0400

    more dumb plotting changes
    please ignore

--- Analysis/drawAudRespIndivTrial.m
+++ Analysis/drawAudRespIndivTrial.m
@@ -27,7 +27,7 @@ titleFSize   = 14;
 axisLSize    = 14;
 lineThick    = 4;
 
-ha = tight_subplot(1, 4, [0.15 0.05],[0.12 0.15],[0.05 0.05]);
+ha = tight_subplot(1, 4, [0.15 0.05],[0.12 0.15],[0.05 0.03]);
 
 for ii = 1:numPT      
     axes(ha(ii))
