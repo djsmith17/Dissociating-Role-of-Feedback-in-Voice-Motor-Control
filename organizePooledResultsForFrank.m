1229b714c3c161b2849095d4403467da21bc637a
Dante Smith <djsmith@bu.edu>
2018-12-05 23:09:59 -0500

    new update to what Frank wants
    stimulus alongside the data instead of time. also not in cents..but in ratio to baseline f0

--- organizePooledResultsForFrank.m
+++ organizePooledResultsForFrank.m
@@ -1,16 +1,36 @@
 function organizePooledResultsForFrank(dirs, allSubjRes)
 
-time          = allSubjRes.secTime';
+time = allSubjRes.secTime';
+zeroPt = 501;
+rampDnPt = 611;
+rampUpPt = 651;
+
+rampDnLen = length(zeroPt:rampDnPt);
+rampUpLen = length(zeroPt:rampUpPt);
+
+finalpert = 2^(-100/1200);
+
+set = ones(length(time), 1);
+
+setOnset = set;
+setOnset(rampDnPt:end) = finalpert;
+setOnset(zeroPt:rampDnPt) = linspace(1, finalpert, rampDnLen);
+
+setOffset = set;
+setOffset(1:zeroPt) = finalpert;
+setOffset(zeroPt:rampUpPt) = linspace(finalpert, 1, rampUpLen);
+
+
 pertResponses = allSubjRes.audioMf0SecPert{1};
 
 onsets  = pertResponses(:, :, 1);
 offsets = pertResponses(:, :, 2);
 
-onsetsRatio  = exp(onsets/1200);
-offsetsRatio = exp(offsets/1200);
+onsetsRatio  = 2.^(onsets/1200);
+offsetsRatio = 2.^(offsets/1200);
 
-onsetSet  = [time, onsetsRatio];
-offsetSet = [time, offsetsRatio];
+onsetSet  = [setOnset, onsetsRatio];
+offsetSet = [setOffset, offsetsRatio];
 
 dirs.alignedMicResponsesFile_Onset  = fullfile(dirs.SavResultsDir, 'Pitch-Shift Reflex Aligned Mic Recordings_Onset.csv');
 dirs.alignedMicResponsesFile_Offset = fullfile(dirs.SavResultsDir, 'Pitch-Shift Reflex Aligned Mic Recordings_Offset.csv');
