1fad06c72a41c61bbb067c95f3e21041333a076f
Dante Smith <djsmith@bu.edu>
2018-12-09 00:29:25 -0500

    making the baseline creation more legible
    sampling rates were all over the place. I simplified things.

--- JND/TokenGeneration/dfGenerateBT.m
+++ JND/TokenGeneration/dfGenerateBT.m
@@ -18,55 +18,49 @@ else
     auto = varargin{1};
 end
 
-baseTrial = GT.baseTrial; % Trial number from the baseline recordings to use.
-tokenL    = GT.tokenLen;  % The target token length
-fs        = GT.fs;        % The sampling rate we want the tokens played at.
+baseTokenFile = dirs.baseTokenFile;
 
-baseTokenFile = dirs.baseTokenFile; % Where to save the .wav file
+BVDataMic = GT.BVDataMic;
+BVfs      = GT.BVfs;
 
-if ~exist(dirs.BaseFile, 'file')
-    error('ERROR: No baseline voice file at the designated location!')
-else
-    fprintf('Loading Baseline recording from...\n%s\n\n', dirs.BaseFile)
-    load(dirs.BaseFile) % Returns a structure called 'DRF'
-end
-    
-BVData  = DRF.rawData(baseTrial); % Grab a specific trial
-baseRec = BVData.signalIn;        % Grab the microphone channel.
-fsRec   = DRF.expParam.sRateAnal; % Rate it was recorded at
-recLen  = length(baseRec);        % points
-recDur  = recLen/fsRec;           % seconds
+fs        = GT.fs;         % The sampling rate to play the tokens at.
+tokenLenP = GT.tokenLenP;  % The token length in points.
+
+% Resample the microphone recording to the sampling rate we intend to play
+% it at
+BVDataMicRS = resample(BVDataMic, fs, BVfs);
+
+recLen  = length(BVDataMicRS);     % points
+recDur  = recLen/fs;           % seconds
 time    = linspace(0, recDur, recLen); % used for manual plotting
 
-tokenLP     = tokenL*fsRec;      % points at rate of recording, not audio play rate. 
 riseTime    = .05;               % seconds
 fallTime    = .05;               % seconds
-riseTimeP   = riseTime*fsRec;    % points
-fallTimeP   = fallTime*fsRec;    % points
+riseTimeP   = riseTime*fs;    % points
+fallTimeP   = fallTime*fs;    % points
 riseQperiod = (4*riseTime)^-1;
 fallQperiod = (4*fallTime)^-1;
 
-window = ones(1, tokenLP);
+window = ones(1, tokenLenP);
 window(1:riseTimeP) = sin(2*pi*riseQperiod*linspace(0, riseTime, riseTimeP)).^2;
-window(tokenLP-fallTimeP + 1:tokenLP) = cos(2*pi*fallQperiod*linspace(0, fallTime, fallTimeP)).^2;
+window(tokenLenP-fallTimeP + 1:tokenLenP) = cos(2*pi*fallQperiod*linspace(0, fallTime, fallTimeP)).^2;
 
 if auto == 1 % Automatic sectioning
     stT = 2.0;
-    ix1 = fsRec*stT;
-    ix2 = ix1 + tokenLP - 1;
+    ix1 = fs*stT;
+    ix2 = ix1 + tokenLenP - 1;
 else         % Manual sectioning
     figure
-    plot(time, baseRec, 'b'); ylim([-1 1])
+    plot(time, BVDataMicRS, 'b'); ylim([-1 1])
     
     [x, ~] = ginput(1);
-    ix1 = round(x(1)*fsRec); % Choose a single point with roughly .5s following it
-    ix2 = ix1 + tokenLP - 1;
+    ix1 = round(x(1)*fs); % Choose a single point with roughly .5s following it
+    ix2 = ix1 + tokenLenP - 1;
     close all
 end
 
-BTraw = baseRec(ix1:ix2);         % raw baseline token (BT)
-BTw   = BTraw.*window';           % windowed 
-BT    = resample(BTw, fs, fsRec); % resampled at 44.1kHz
+BTraw = BVDataMicRS(ix1:ix2);    % raw baseline token (BT)
+BT    = BTraw.*window';          % windowed 
 
 audiowrite(baseTokenFile, BT, fs) % saved to tokens folder 
 end
