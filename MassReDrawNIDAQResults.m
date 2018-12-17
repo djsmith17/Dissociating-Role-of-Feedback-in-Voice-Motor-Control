86161fd2d3ad93268002912866d2abdde1d0bd1c
Dante Smith <djsmith@bu.edu>
2018-12-17 16:07:31 -0500

    update to my quick function for updating my pilot plots
    working on making my NIDAQ recordings a bit easier to work with . Just a bit of cleanup to allow this to work a bit smoother

--- MassReDrawNIDAQResults.m
+++ MassReDrawNIDAQResults.m
@@ -1,13 +1,26 @@
 
+project = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control';
+
 exp = 'NewSensorTest_Winter18';
-run = {'DS5', 'DS6', 'DS7', 'DS8', 'DS9', 'DS10', 'DS11', 'DS12', 'DS13', 'DS14', 'DS15', 'DS16', 'DS17'};
+runs = {'DS5', 'DS6', 'DS7', 'DS8', 'DS9', 'DS10', 'DS11', 'DS12', 'DS13', 'DS14', 'DS15', 'DS16', 'DS17'};
 numRuns = length(runs);
 
-dirOpen = 'W:\Experiments\Dissociating-Role-of-Feedback-in-Voice-Motor-Control\NewSensorTest_Winter2018';
+dirs = dfDirs(project);
 
-dirSave = 'E:\Documents\MATLAB\dfResults\Results';
+dirs.SavedResultsDir = fullfile(dirs.Results, exp);
 
-for i = 1:numRuns
+f0b = 100;
+pF  = 1;
+iRF = 0;
+
+for ii = 1:numRuns
+    curRun = runs{ii};
+    dirs.SavedFileDir = fullfile(dirs.SavData, exp, curRun);
+    theFile           = fullfile(dirs.SavedFileDir, [exp curRun 'NSD.mat']);
     
-    
+    load(theFile) % returns NSD    
+
+    [niAn, niRes] = dfAnalysisNIDAQ(dirs, NSD.expParam, NSD.DAQin, f0b, 0, iRF, pF);        
+    drawDAQAlignedPressure(niRes, dirs.SavResultsDir, 1)
+    close all
 end
