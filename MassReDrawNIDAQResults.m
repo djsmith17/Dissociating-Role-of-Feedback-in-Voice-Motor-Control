fc81b8b61b7abc0a3871041b71ff6ab787cdc04b
djsmith17 <djsmith@bu.edu>
2018-12-17 00:00:24 -0500

    quick script for checking plots

---
+++ MassReDrawNIDAQResults.m
@@ -0,0 +1,13 @@
+
+exp = 'NewSensorTest_Winter18';
+run = {'DS5', 'DS6', 'DS7', 'DS8', 'DS9', 'DS10', 'DS11', 'DS12', 'DS13', 'DS14', 'DS15', 'DS16', 'DS17'};
+numRuns = length(runs);
+
+dirOpen = 'W:\Experiments\Dissociating-Role-of-Feedback-in-Voice-Motor-Control\NewSensorTest_Winter2018';
+
+dirSave = 'E:\Documents\MATLAB\dfResults\Results';
+
+for i = 1:numRuns
+    
+    
+end
