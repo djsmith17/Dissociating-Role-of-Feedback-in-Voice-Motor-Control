d54e01bec1540533cd494824e917d11d3fdaf260
djsmith17 <djsmith@bu.edu>
2019-01-18 18:46:30 -0500

    moving the stats out of the pooledAnalysis pipeline
    since the stats of a given study wil be dependent on what the data look like, it doesnt make sense to keep this in the whole pipeline. Moving it out so I can make it more modular

--- Analysis/dfRunPooledAnalysis.m
+++ Analysis/dfRunPooledAnalysis.m
@@ -127,7 +127,7 @@ organizeAndPrintDemographicStats(allSubjRes);
 % organizeAndSaveExcludedTrialTable(dirs, pA, allSubjRes, tossTrialTracker, tVN, 0)
 
 % Organize and Save the Table of Output Variables
-organizeAndSaveOutputVariables(dirs, pA, allSubjRes);    
+StatsOrg_MaskingNoiseStudy(dirs, pA, allSubjRes);    
 
 % Save the Pooled Results
 dirs.SavResultsFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'ResultsDRF.mat']);
@@ -718,99 +718,3 @@ if svFile == 1
 end
 end
 
-function organizeAndSaveOutputVariables(dirs, pA, allSubjRes)
-
-dirs.behavioralResultTable = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'BehavioralResultTable.xlsx']);
-
-allSubjStatTable = allSubjRes.statTable;
-n = height(allSubjStatTable);
-meas = {'StimMag', 'RespMag', 'RespPer'};
-units = {'cents', 'cents', '%'};
-colors = ['b', 'r', 'g'];
-mu     = '\mu';
-sigma  = '\sigma';
-nMeas = length(meas);
-
-cond    = pA.cond;
-numCond = pA.numCond;
-
-allMeasureStats = [];
-for k = 1:nMeas
-    curStatTable = allSubjStatTable(:, {'AudFB', meas{k}});
-    
-%     measFit = fitrm(curStatTable, 'StimMag~AudFB');
-%     measSph = mauchly(measFit);
-    
-    measDist = figure('Color', [1 1 1]);
-    plotpos = [10 10]; plotdim = [1300 800];
-    set(measDist, 'Position',[plotpos plotdim],'PaperPositionMode','auto')
-    ha = tight_subplot(2, numCond,[0.1 0.05],[0.1 0.1],[0.05 0.05]);
-
-    measStats = [];
-    for i = 1:numCond
-        curFB = strcmp(curStatTable.AudFB, cond(i));
-        
-        measure   = curStatTable{curFB, 2};        
-        numObs    = length(measure);
-        
-        measureTrans = log10(measure + 1 - min(measure));
-        
-        measureM  = round(mean(measureTrans), 2);
-        measureMed = round(median(measureTrans), 2);
-        measureMin = round(min(measureTrans), 2);
-        measureMax = round(max(measureTrans), 2);
-        measureSD = round(std(measureTrans), 2);
-        measureSE = round(measureSD/sqrt(numObs), 2);
-        
-        measureSkew     = round(skewness(measure), 4);
-        measureKurotsis = round(kurtosis(measure), 2);
-        
-        measureZScore   = (measureTrans-measureM)./measureSD;
-        [swH, swPValue, swTest] = swtest(measureZScore);
-        
-        swPValue = round(swPValue, 3);
-        swTest   = round(swTest, 3);
-        
-        measStat = [measureM;...
-                    measureMin;...
-                    measureMed;...
-                    measureMax;...
-                    measureSD;...
-                    measureSE;...
-                    measureSkew;...
-                    measureKurotsis;...
-                    swH;...
-                    swPValue;...
-                    swTest];
-        measStats = cat(2, measStats, measStat);
-        
-        step = 25;
-        minBound = floor(measureMin/step)*step;
-        maxBound = ceil(measureMax/step)*step;
-%         distBin = minBound:step:maxBound;
-%         nBins = length(distBin)-1;
-        
-        axes(ha(i))
-        histogram(measureTrans, 'FaceColor', colors(i), 'EdgeColor', colors(i))
-        title(cond{i})
-        box off;
-        
-        axes(ha(i+numCond))
-        cdfplot(measureZScore)
-        hold on
-        xValues = linspace(min(measureZScore), max(measureZScore));
-        plot(xValues, normcdf(xValues, 0, 1), 'r-')
-        legend('Empirical CDF','Standard Normal CDF','Location','best')        
-    end
-    suptitle({pA.pAnalysis, meas{k}})    
-    
-    allMeasureStats = cat(2, allMeasureStats, measStats);
-    dirs.DistributionFigureFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis meas{k} 'DistributionPlotTrans.jpg']);
-    export_fig(dirs.DistributionFigureFile)
-    
-    xlswrite(dirs.behavioralResultTable, measStats, meas{k})
-end
-
-fullResultFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'AllVariableTable.xlsx']);
-writetable(allSubjStatTable, fullResultFile, 'WriteVariableNames',true)
-end
