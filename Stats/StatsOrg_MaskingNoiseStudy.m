b6db8b89552e3b576a23b44e628cbe2f26f61c91
Dante Smith <djsmith@bu.edu>
2019-01-23 12:35:03 -0500

    clean up on aisle stats
    just moving things into more compartmentalized functions so I can follow this flow a bit better, and it doesnt need to be as linear. Most importantly, the histogram is its own separate function which means I can do tests of transformation without breaking up this process.

--- Stats/StatsOrg_MaskingNoiseStudy.m
+++ Stats/StatsOrg_MaskingNoiseStudy.m
@@ -4,111 +4,65 @@ dirs.behavioralResultTable = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'Behavio
 
 allSubjStatTable = allSubjRes.statTable;
 meas = {'StimMag', 'RespMag', 'RespPer'};
-units = {'cents', 'cents', '%'};
-colors = ['b', 'r', 'g'];
-mu     = '\mu';
-sigma  = '\sigma';
-nMeas = length(meas);
 
 cond    = pA.cond;
 numCond = pA.numCond;
 
 curTestingMeas = 1;
-
-allMeasureStats = [];
 for k = curTestingMeas
     curStatTable = allSubjStatTable(:, {'AudFB', meas{k}});
-    
-    measDist = figure('Color', [1 1 1]);
-    plotpos = [10 10]; plotdim = [1300 800];
-    set(measDist, 'Position',[plotpos plotdim],'PaperPositionMode','auto')
-    ha = tight_subplot(2, numCond,[0.1 0.05],[0.1 0.1],[0.05 0.05]);
 
-    measStats = [];
+    measureSummaryStrs     = [];
+    measureTAcrossCond     = [];
+    variableStatAcrossCond = [];
     for i = 1:numCond
         curFB = strcmp(curStatTable.AudFB, cond(i));
         
+        % Identify the Variable and Condition
         measure   = curStatTable{curFB, 2};
         
-        [summaryStr, pooledVariable] = RawSummaryStats(measure);
+        % Perform Standard Sumamry Stats
+        [summaryStr, variableStat] = RawSummaryStats(meas{k}, measure);
         
         if k == 4
             summaryStr.measureT = boxcox(summaryStr.measure);
-            suffix = 'Trans';
+            summaryStr.isTrans = 1;
+            summaryStr.suffix  = 'Trans';
         else
             summaryStr.measureT = summaryStr.measure;
-            suffix = '';
+            summaryStr.isTrans = 0;
+            summaryStr.suffix  = '';
         end
-                
-        summaryStr.measureSkew     = round(skewness(summaryStr.measureT), 4);
-        summaryStr.measureKurtosis = round(kurtosis(summaryStr.measureT), 2);
+             
+        % Use some function to describe the normality
+        summaryStr = testNormality(summaryStr);
         
-        summaryStr.measureZ        = zscore(summaryStr.measureT);
-        [swH, swPValue, swTest]    = swtest(summaryStr.measureZ);
+        % Add the Normality results to the variableStat Array
+        variableStat = concateAdditionalStat(summaryStr, variableStat);
         
-        summaryStr.swH      = swH;
-        summaryStr.swPValue = round(swPValue, 3);
-        summaryStr.swTest   = round(swTest, 3);
-        
-        pooledVariable = concateAdditionalStat(summaryStr, pooledVariable);
-        
-        measStat = [measureM;...
-                    measureMin;...
-                    measureMed;...
-                    measureMax;...
-                    measureSD;...
-                    measureSE;...
-                    measureSkew;...
-                    measureKurotsis;...
-                    swH;...
-                    swPValue;...
-                    swTest];
-        measStats = cat(2, measStats, summaryStr);
-        
-        step = 25;
-        minBound = floor(measureMin/step)*step;
-        maxBound = ceil(measureMax/step)*step;
-        distBin = minBound:step:maxBound;
-        nBins   = length(distBin)-1;
-        
-        axes(ha(i))
-        if k == 4
-            histogram(measureTrans, 8, 'FaceColor', colors(i), 'EdgeColor', colors(i))
-        else
-            histogram(measureTrans, nBins, 'FaceColor', colors(i), 'EdgeColor', colors(i), 'BinLimits', [minBound maxBound])
-        end
-        title(cond{i})
-        box off;
-        
-        axes(ha(i+numCond))
-        cdfplot(measureZScore)
-        hold on
-        xValues = linspace(min(measureZScore), max(measureZScore));
-        plot(xValues, normcdf(xValues, 0, 1), 'r-')
-        legend('Empirical CDF','Standard Normal CDF','Location','best')        
+        % Concatenate the Summary Stat Arrays across condition
+        variableStatAcrossCond = cat(2, variableStatAcrossCond, variableStat);
+
+        % Concatenate the Structure for Histogram and Transformed Values
+        measureSummaryStrs = cat(1, measureSummaryStrs, summaryStr);      
     end
-    suptitle({pA.pAnalysis, meas{k}})
-    
+    plotHistograms(measureSummaryStrs, dirs, pA)
 %     measFit = fitrm(curStatTable, 'RespMag~AudFB');
 %     measSph = mauchly(measFit);
     
-    
-    allMeasureStats = cat(2, allMeasureStats, measStats);
-    dirs.DistributionFigureFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis meas{k} suffix 'DistributionPlot.jpg']);
-    export_fig(dirs.DistributionFigureFile)
-    
-    xlswrite(dirs.behavioralResultTable, measStats, meas{k})
+    xlswrite(dirs.behavioralResultTable, variableStatAcrossCond, meas{k})
 end
 
 fullResultFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'AllVariableTable.xlsx']);
 writetable(allSubjStatTable, fullResultFile, 'WriteVariableNames',true)
 end
 
-function [summaryStr, pooledVariable] = RawSummaryStats(measure)
+function [summaryStr, variableStat] = RawSummaryStats(variableName, measure)
 
 numObs    = length(measure);
 
 % Calculate the Descriptive Stats
+summaryStr.varName  = variableName;
 summaryStr.measure  = measure;        % Raw Data Values
 summaryStr.measureT = [];             % Transformed Data Values
 summaryStr.measureZ = [];             % Z-Scored Data Values 
@@ -119,21 +73,85 @@ summaryStr.max      = round(max(measure), 2);
 summaryStr.SD       = round(std(measure), 2);
 summaryStr.SE       = round(summaryStr.SD/sqrt(numObs), 2);
 
-pooledVariable = {summaryStr.mean;...
-                  summaryStr.median;...
-                  summaryStr.min;...
-                  summaryStr.max;...
-                  summaryStr.SD;...
-                  summaryStr.SE};
+variableStat = {summaryStr.mean;...
+                summaryStr.median;...
+                summaryStr.min;...
+                summaryStr.max;...
+                summaryStr.SD;...
+                summaryStr.SE};
                   
 
 end
 
-function pooledVariable = concateAdditionalStat(summaryStr, pooledVariable)
+function summaryStr = testNormality(summaryStr)
 
-pooledVariable = cat(1, pooledVariable, summaryStr.measureSkew);
-pooledVariable = cat(1, pooledVariable, summaryStr.measureKurtosis);
-pooledVariable = cat(1, pooledVariable, summaryStr.swH);
-pooledVariable = cat(1, pooledVariable, summaryStr.swPValue);
-pooledVariable = cat(1, pooledVariable, summaryStr.swTest);
+% Skew and Kurtosis
+summaryStr.measureSkew     = round(skewness(summaryStr.measureT), 4);
+summaryStr.measureKurtosis = round(kurtosis(summaryStr.measureT), 2);
+
+% Z-Score and Shapiro-Wilk Test
+summaryStr.measureZ        = zscore(summaryStr.measureT);
+[swH, swPValue, swTest]    = swtest(summaryStr.measureZ);
+
+summaryStr.swH      = double(swH);
+summaryStr.swPValue = round(swPValue, 3);
+summaryStr.swTest   = round(swTest, 3);
+end
+
+function variableStat = concateAdditionalStat(summaryStr, variableStat)
+
+variableStat = cat(1, variableStat, summaryStr.measureSkew);
+variableStat = cat(1, variableStat, summaryStr.measureKurtosis);
+variableStat = cat(1, variableStat, summaryStr.swH);
+variableStat = cat(1, variableStat, summaryStr.swPValue);
+variableStat = cat(1, variableStat, summaryStr.swTest);
+end
+
+function plotHistograms(measureSummaryStrs, dirs, pA)
+
+units  = {'cents', 'cents', '%'};
+colors = ['b', 'r', 'g'];
+mu     = '\mu';
+sigma  = '\sigma';
+
+cond    = pA.cond;
+numCond = pA.numCond;
+
+measDist = figure('Color', [1 1 1]);
+plotpos = [10 10]; plotdim = [1300 800];
+set(measDist, 'Position',[plotpos plotdim],'PaperPositionMode','auto')
+ha = tight_subplot(2, numCond,[0.1 0.05],[0.1 0.1],[0.05 0.05]);
+
+for ii = 1:numCond
+    summaryStr = measureSummaryStrs(ii);
+    
+    varName = summaryStr.varName; % Should be the same each time, but this is easier
+    suffix  = summaryStr.suffix;  % Should be the same each time, but this is easier
+    
+    step = 25;
+    minBound = floor(summaryStr.min/step)*step;
+    maxBound = ceil(summaryStr.max/step)*step;
+    distBin  = minBound:step:maxBound;
+    nBins    = length(distBin)-1;
+
+    axes(ha(ii))
+    if summaryStr.isTrans
+        histogram(summaryStr.measureT, 8, 'FaceColor', colors(ii), 'EdgeColor', colors(ii))
+    else
+        histogram(summaryStr.measureT, nBins, 'FaceColor', colors(ii), 'EdgeColor', colors(ii), 'BinLimits', [minBound maxBound])
+    end
+    title(cond{ii})
+    box off;
+
+    axes(ha(ii+numCond))
+    cdfplot(summaryStr.measureZ)
+    hold on
+    xValues = linspace(min(summaryStr.measureZ), max(summaryStr.measureZ));
+    plot(xValues, normcdf(xValues, 0, 1), 'r-')
+    legend('Empirical CDF','Standard Normal CDF','Location','best') 
+end
+suptitle({pA.pAnalysis, varName})
+
+dirs.DistributionFigureFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis varName suffix 'DistributionPlot.jpg']);
+export_fig(dirs.DistributionFigureFile)
 end
