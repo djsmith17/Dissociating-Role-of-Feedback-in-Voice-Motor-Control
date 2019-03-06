f87b5c1be13d76d6a2b5da0f49ef904fefb51d6d
Dante Smith <djsmith@bu.edu>
2019-03-06 15:32:47 -0500

    beginning to check over analysis struct

--- Analysis/dfRunSubjAnalysis.m
+++ Analysis/dfRunSubjAnalysis.m
@@ -16,12 +16,12 @@ function dfRunSubjAnalysis()
 
 close all
 AVar.project       = 'Dissociating-Role-of-Feedback-in-Voice-Motor-Control';
-AVar.participants  = {'DRF_MN6'}; % List of multiple participants.
+AVar.participants  = {'DRF1'}; % List of multiple participants.
 AVar.numPart       = length(AVar.participants);
-AVar.runs          = {'SF3'}; %    List of multiple runs.
+AVar.runs          = {'SF1'}; %    List of multiple runs.
 AVar.numRuns       = length(AVar.runs);
 AVar.baselineFile  = 'BV1';            % Baseline Voice information
-AVar.debug         = 0;
+AVar.debug         = 1;
 
 dirs               = dfDirs(AVar.project);
 dirs.LoadData      = dirs.SavData;
