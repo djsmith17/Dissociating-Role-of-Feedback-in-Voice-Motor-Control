b9a974fa6554355df995046ccec9c097bdee5d44
djsmith17 <djsmith@bu.edu>
2019-03-20 04:44:20 -0400

    Created some prettier plots
    These plots look great and so do you

--- Analysis/dFGenPostHocAnalysis.m
+++ Analysis/dFGenPostHocAnalysis.m
@@ -1,4 +1,4 @@
-function dFGenPostHocAnalysis()
+function dfGenPostHocAnalysis()
 
 close all
 
@@ -47,10 +47,24 @@ end
 
 function drawQuest2(dirs, respPer_SomVF, respPer_SomMN, respPer_Aud)
 
-figure
+plotpos = [10 100];
+plotdim = [1200 500];
+q2Fig = figure('Color', [1 1 1]);
+set(q2Fig, 'Position', [plotpos plotdim],'PaperPositionMode','auto')
+
 plot(respPer_SomVF, 'bo-'); hold on
 plot(respPer_SomMN, 'ro-'); 
 plot(respPer_Aud, 'go-');
+xlabel('Participant')
+ylabel('RespPer (%)')
+title('Comparison of Response Percentage between Experimental Conditions')
+box off
+
+legend({'Somato Feedback Pert (Voice Feedback)', 'Somato Feedback Pert (Masking Noise)', 'Aud Feedback Pert'},...
+        'Box', 'off',...
+        'Edgecolor', [1 1 1],...
+        'FontSize', 12,...
+        'FontWeight', 'bold')
 
 dirs.quest2FigFile = fullfile(dirs.SavResultsDir, 'Question2.jpg');
 export_fig(dirs.quest2FigFile)
@@ -58,8 +72,15 @@ end
 
 function drawQuest3(dirs, respPer_SomVF, JNDScore)
 
-figure
+plotpos = [10 100];
+plotdim = [900 500];
+q3Fig = figure('Color', [1 1 1]);
+set(q3Fig, 'Position', [plotpos plotdim],'PaperPositionMode','auto')
+
 plot(JNDScore, respPer_SomVF, 'ko')
+xlabel('JND Score')
+ylabel('RespPer (%)')
+title('Relationship between Somato Pert RespPer and JND Score')
 
 dirs.quest3FigFile = fullfile(dirs.SavResultsDir, 'Question3.jpg');
 export_fig(dirs.quest3FigFile)
@@ -67,8 +88,15 @@ end
 
 function drawQuest4(dirs, respPer_Aud, JNDScore)
 
-figure
+plotpos = [10 100];
+plotdim = [900 500];
+q4Fig = figure('Color', [1 1 1]);
+set(q4Fig, 'Position', [plotpos plotdim],'PaperPositionMode','auto')
+
 plot(JNDScore, respPer_Aud, 'ko')
+xlabel('JND Score')
+ylabel('RespPer (%)')
+title('Relationship between Audio Pert RespPer and JND Score')
 
 dirs.quest4FigFile = fullfile(dirs.SavResultsDir, 'Question4.jpg');
 export_fig(dirs.quest4FigFile)
