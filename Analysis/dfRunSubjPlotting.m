fa8021986bcd6d55d4a2455a5e683b073f04c557
djsmith17 <djsmith@bu.edu>
2019-03-19 22:04:31 -0400

    cleaning up the logic and words for plotting
    needed to keep track of which plotting functions I am using

--- Analysis/dfRunSubjPlotting.m
+++ Analysis/dfRunSubjPlotting.m
@@ -22,10 +22,10 @@ ext                = '';
 sv2File                      = 1;
 sPlt.drawDAQAll              = 0; % All signals recorded by the NIDAQ
 sPlt.drawDAQPresMic          = 0; % Pressure vs Microphone Data
-sPlt.drawDAQAlignedPressure  = 1; % Superimposed Pressure recordings from perturbed trials
-sPlt.drawMeanTrial_PertCont  = 1; % Mean Trials Microphone input. Control vs Perturbed Trials
-sPlt.drawAllTrial_Pert       = 1; % All Perturbed Trials Microphone input
-sPlt.drawMeanTrial_MicHead   = 0; % Mean Perturbed Trials. Microphone vs Headphones
+
+sPlt.drawDAQAlignedPressure  = 0; % Superimposed Pressure recordings from perturbed trials
+sPlt.drawPertMicResponse     = 0; % All Perturbed Trials Microphone input
+sPlt.drawMicHeadResponse     = 0; % Mean Perturbed Trials. Microphone vs Headphones
 
 presFlag = 1;
  
@@ -57,15 +57,12 @@ for ii = 1:sPlt.numPart
             drawDAQAlignedPressure(res, dirs.PlotResultsDir, sv2File)
         end
         
-        if sPlt.drawMeanTrial_PertCont == 1
+        if sPlt.drawPertMicResponse == 1
             drawMeanTrialMicf0(res, dirs.PlotResultsDir, presFlag)
-        end
-        
-        if sPlt.drawAllTrial_Pert == 1
             drawAllPertTrialMicf0(res, dirs.PlotResultsDir, presFlag)
         end            
         
-        if sPlt.drawMeanTrial_MicHead == 1
+        if sPlt.drawMicHeadResponse == 1
             drawAudRespIndivTrial(res, dirs.PlotResultsDir)
             drawAudRespMeanTrial(res, dirs.PlotResultsDir)
         end
