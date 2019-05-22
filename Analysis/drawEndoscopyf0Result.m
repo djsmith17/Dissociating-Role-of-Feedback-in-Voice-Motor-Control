f4981dc2bed351a81661172abc73c1e00b803f67
djsmith17 <djsmith@bu.edu>
2019-05-22 19:14:17 -0400

    EndoAnalysis Plotting Update
    Moving some functions around, and making it more modular

--- Analysis/drawEndoscopyf0Result.m
+++ Analysis/drawEndoscopyf0Result.m
@@ -3,10 +3,19 @@ function drawEndoscopyf0Result()
 close all
 dirs = dfDirs('Dissociating-Role-of-Feedback-in-Voice-Motor-Control');
 
-participant = 'DRF19';
-run         = 'SFL1';
-curTrial    = 8;
+allParti = {'DRF5', 'DRF9', 'DRF12', 'DRF14', 'DRF19'};
+eachTrial = [3 10 8 5 8];
 
+for ii = 1:5
+    participant = allParti{ii};
+    run         = 'SFL1';
+    curTrial    = eachTrial(ii);
+    analyzeAndDrawResult(dirs, participant, run, curTrial)
+end
+end
+
+function analyzeAndDrawResult(dirs, participant, run, curTrial)
+        
 resultParticipantDir = fullfile(dirs.Results, participant, run);
 behavResultsFile     = fullfile(resultParticipantDir, [participant run 'ResultsDRF.mat']);
 videoCodResultsFile  = fullfile(resultParticipantDir, [participant run 'EndoFrameMeasuresDJS.mat']);
@@ -29,14 +38,15 @@ pertColor = [0.8 0.8 0.8];
 ii = find(trialNums == curTrial);
 
 plotpos = [10 0];
-plotdim = [1000 1000];
+plotdim = [1000 500];
 InterTrialf0 = figure('Color', [1 1 1]);
 set(InterTrialf0, 'Position',[plotpos plotdim],'PaperPositionMode','auto')
 
 pertAx  = [pertTrig(ii,1), pertTrig(ii,2)];
 pertAy  = [600 600];
 
-subplot(4,1,1)
+% f0 Results
+subplot(2,1,1)
 yyaxis right
 plot(timePres, sensorP(:,ii), '--k', 'LineWidth', 1.5)
 ylabel('Pressure (psi)')
@@ -52,12 +62,13 @@ plot(time, sigs(:,ii), 'b', 'LineWidth', 2)
 ylabel('f0 (cents)', 'FontSize', 18, 'FontWeight', 'bold')
 title({[participant ' ' run],[ ' Trial ' num2str(trialNums(ii))]}, 'FontSize', 18, 'FontWeight', 'bold')
 axis(limits); box off
-legend(pA, 'Perturbation Period', 'box', 'off')
+lgd = legend(pA, 'Perturbation Period');
+lgd.EdgeColor = 'none';
 
 set(gca,'FontSize', 14,...
         'FontWeight','bold')
   
-% Video Results
+% Video Results:
 curTable = CodedEndoFrameDataSet{ii};
 
 timeFrames = linspace(0,4,121);
@@ -65,44 +76,48 @@ pt1X = curTable.FidPt1X;
 pt2X = curTable.FidPt2X;
 pt1Y = curTable.FidPt1Y;
 pt2Y = curTable.FidPt2Y;
-dist = curTable.Dist;
+distRaw = curTable.Dist;
+[dist, distLim] = adjustDistMeasure(timeFrames, distRaw);
+
 XPtsDiff = pt2X - pt1X;
 YPtsDiff = pt2Y - pt1Y;
 
-subplot(4,1,2)
+% %X Component
+% subplot(2,1,2)
+% pA = area(pertAx, pertAy, -600, 'FaceColor', pertColor, 'EdgeColor', pertColor);
+% hold on
+% pt1XLn = plot(timeFrames, pt1X, 'm');
+% hold on
+% pt2XLn = plot(timeFrames, pt2X, 'g');
+% axis([0 4 180 480]); box off
+% ylabel('X Position (Pixel)')
+% title('X Component over time')
+% legend([pt1XLn, pt2XLn], 'Point1', 'Point2', 'box', 'off', 'Orientation', 'horizontal')
+% 
+% set(gca,'FontSize', 14,...
+%         'FontWeight','bold')
+% 
+% subplot(2,1,3)
+% pA = area(pertAx, pertAy, -600, 'FaceColor', pertColor, 'EdgeColor', pertColor);
+% hold on
+% pt1YLn = plot(timeFrames, pt1Y, 'm');
+% hold on
+% pt2YLn = plot(timeFrames, pt2Y, 'g');
+% axis([0 4 150 350]); box off
+% set(gca,'Ydir','reverse')
+% ylabel('Y Position (Pixel)')
+% title('Y Component over time')
+% legend([pt1YLn, pt2YLn], 'Point1', 'Point2', 'box', 'off', 'Orientation', 'horizontal')
+% 
+% set(gca,'FontSize', 14,...
+%         'FontWeight','bold')
+
+subplot(2,1,2)
 pA = area(pertAx, pertAy, -600, 'FaceColor', pertColor, 'EdgeColor', pertColor);
 hold on
-pt1XLn = plot(timeFrames, pt1X, 'm');
-hold on
-pt2XLn = plot(timeFrames, pt2X, 'g');
-axis([0 4 180 480]); box off
-ylabel('X Position (Pixel)')
-title('X Component over time')
-legend([pt1XLn, pt2XLn], 'Point1', 'Point2', 'box', 'off', 'Orientation', 'horizontal')
-
-set(gca,'FontSize', 14,...
-        'FontWeight','bold')
-
-subplot(4,1,3)
-pA = area(pertAx, pertAy, -600, 'FaceColor', pertColor, 'EdgeColor', pertColor);
-hold on
-pt1YLn = plot(timeFrames, pt1Y, 'm');
-hold on
-pt2YLn = plot(timeFrames, pt2Y, 'g');
-axis([0 4 150 350]); box off
-set(gca,'Ydir','reverse')
-ylabel('Y Position (Pixel)')
-title('Y Component over time')
-legend([pt1YLn, pt2YLn], 'Point1', 'Point2', 'box', 'off', 'Orientation', 'horizontal')
-
-set(gca,'FontSize', 14,...
-        'FontWeight','bold')
-
-subplot(4,1,4)
-pA = area(pertAx, pertAy, -600, 'FaceColor', pertColor, 'EdgeColor', pertColor);
-hold on
+plot([-1 5], [0 0], '--k')
 ptDistLn = plot(timeFrames, dist, 'b');
-axis([0 4 220 260]); box off
+axis([0 4 distLim]); box off
 xlabel('Time (s)', 'FontSize', 18, 'FontWeight', 'bold')
 ylabel('Distance (Pixels)')
 title('Euclidian Distance Between Points')
@@ -113,4 +128,17 @@ set(gca,'FontSize', 14,...
 fileName = 'EndoDistanceMeasure.png';
 savedFigFile = fullfile(resultParticipantDir, [participant run fileName]);
 export_fig(savedFigFile)
+end
+
+function [dist, distLim] = adjustDistMeasure(time, distRaw)
+
+indPrePert = time < 1.0;
+
+distSmooth = smooth(distRaw, 3);
+distBase   = mean(distSmooth(indPrePert));
+
+dist = distSmooth - distBase;
+distMax = max(dist) + 10;
+distMin = min(dist) - 10;
+distLim = [distMin distMax];
 end
