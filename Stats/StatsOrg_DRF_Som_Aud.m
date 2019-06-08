899bffae171f341ccfccbbdd663c53535dbb5ce5
Dante Smith <djsmith@bu.edu>
2019-06-07 22:16:24 -0400

    Updating Som and Aud comparison
    based on the super easy class I made for doing these measures.

--- Stats/StatsOrg_DRF_Som_Aud.m
+++ Stats/StatsOrg_DRF_Som_Aud.m
@@ -2,10 +2,12 @@ function StatsOrg_DRF_Som_Aud(dirs, StatTableSomVF, StatTableSomMN, StatTableAud
 % q4: Do participants show similar compensatory respones when only Auditory
 % feedback is perturbed? 
 
-pA.pAnalysis = 'Butts';
+pA.pAnalysis = 'DRF_Som_Aud';
+
+dirs.behavioralResultTable = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'BehavioralResultTable.xlsx']);
 
 % Question 4 %%%
-% Currently Expecting Fewer 'Observations' from SomVF and SomMN
+% Currently Expecting Fewer 'Observations' from SomVF/SomMN compared to Aud
 I = ismember(StatTableAud.SubjID, StatTableSomVF.SubjID) == 0;
 StatTableAudLs = StatTableAud;
 StatTableAudLs(I,:) = [];
@@ -32,38 +34,32 @@ else
     lambdas = [0 0 0];
 end
 
-measureSummaryStrs     = [];
-variableStatAcrossCond = [];
+measureSummaryStrs        = [];
+summaryVarTableAcrossCond = table();
 for i = 1:pA.numCond
     % Identify the Variable and Condition
+    curCond   = pA.condName{i};
     measure   = respPerCol(:, i);
 
     % Perform Standard Sumamry Stats
-    [summaryStr, variableStat] = RawSummaryStats('RespPer', measure, lambdas(i));
+    summaryStat = MeasureSummaryStats(dirs, pA, 'RespPer', curCond, measure, lambdas(i));
     
     if ApplyTrans == 1
-        usedLambda = lambdas(2);
-        summaryStr.measureT   = boxcox(usedLambda, summaryStr.measure);
-        summaryStr.isTrans    = 1;
-        summaryStr.suffix     = 'TransVF';
-        summaryStr.usedLambda = num2str(usedLambda);
+        usedLambda  = lambdas(2);
+        summaryStat = summaryStat.performSpecificBoxCoxTrans(usedLambda);
     end
     
-    % Use some function to describe the normality
-    summaryStr = testNormality(summaryStr);
-
-    % Add the Normality results to the variableStat Array
-    variableStat = concateAdditionalStat(summaryStr, variableStat);
+    % Describe the normality
+    summaryStat = summaryStat.testNormality();
 
     % Concatenate the Summary Stat Arrays across condition
-    variableStatAcrossCond = cat(2, variableStatAcrossCond, variableStat);
+    summaryVarTableAcrossCond = [summaryVarTableAcrossCond; summaryStat.SummaryTable];
 
     % Concatenate the Structure for Histogram and Transformed Values
-    measureSummaryStrs = cat(1, measureSummaryStrs, summaryStr);      
+    measureSummaryStrs = cat(1, measureSummaryStrs, summaryStat.SummaryStruct);
 end
 
 drawHistograms(measureSummaryStrs, dirs, pA)
-testNonParametric(measureSummaryStrs)
 
 varCmp = [1 2; 1 3; 2 3];
 varH   = [1.04, 1.12, 0.82];
@@ -71,99 +67,28 @@ comp = length(varCmp);
 
 measureSummaryStrDiff = [];
 for jj = 1:comp
-    
+    cond = ['DiffBetween' num2str(varCmp(jj,:))];
     % Find the difference between the two conditions and place in Struct
     measDiff = measureSummaryStrs(varCmp(jj,1)).measure - measureSummaryStrs(varCmp(jj,2)).measure;
-    [summaryStrDiff, ~] = RawSummaryStats(['Diff'], measDiff, 0);
+    summaryStatDiff = MeasureSummaryStats(dirs, pA, 'RespPer', cond, measDiff, 0);
     
-    summaryStrDiff.vars = varCmp(jj,:);
-    summaryStrDiff.h    = varH(jj);
+    summaryStatDiff.SummaryStruct.vars = varCmp(jj,:);
+    summaryStatDiff.SummaryStruct.h    = varH(jj);
 
-    [summaryStrDiff.measureT, l] = boxcox(summaryStrDiff.measure + 1 - min(summaryStrDiff.measure));
-    summaryStrDiff.isTrans    = 1;
-    summaryStrDiff.suffix     = 'Trans';
-    summaryStrDiff.usedLambda = num2str(round(l,2));
+    % Apply a Box Cox transform with the default (best) lambda
+    summaryStatDiff = performSimpleBoxCoxTrans(summaryStatDiff);
 
-    % Test for Normality
-    summaryStrDiff = testNormality(summaryStrDiff);
+    summaryStatDiff = summaryStatDiff.testNormality();       % Test Normality
+    summaryStatDiff = summaryStatDiff.performTTest();        % Perform t-test
+    summaryStatDiff.drawHistoBoxCombo()                      % Visualize Normality/Outliers
 
-    % Perform a One-Sample T-Test on the difference between the measures
-    [summaryStrDiff.fH, summaryStrDiff.fP] = ttest(summaryStrDiff.measureT);
-    summaryStrDiff.fPround = sprintf('%0.6f', summaryStrDiff.fP);
-    if summaryStrDiff.fP < (0.05/3)
-        summaryStrDiff.isSig = 1;
-    else
-        summaryStrDiff.isSig = 0;
-    end
-
-    drawHistoBoxCombo(summaryStrDiff, dirs, pA) % Visualize the Normality/Outliers of 
+    measureSummaryStrDiff = cat(1, measureSummaryStrDiff, summaryStatDiff.SummaryStruct);
     
-    measureSummaryStrDiff = cat(1, measureSummaryStrDiff, summaryStrDiff);
+    % Save Behavioral Result Table: Values ready for inclusion in manuscript 
+    writetable(summaryStatDiff.statSentTable, dirs.behavioralResultTable, 'Range', 'A7', 'WriteRowNames', 1, 'Sheet', ['RespPer' cond])
 end
 
 drawBoxPlot(dirs, pA, measureSummaryStrs, measureSummaryStrDiff)
-
-% dirs.behavioralResultTable = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'BehavioralResultTable' summaryStr.suffix '.xlsx']);
-% xlswrite(dirs.behavioralResultTable, variableStatAcrossCond, meas{k})
-
-% fullResultFile = fullfile(dirs.SavResultsDir, [pA.pAnalysis 'AllVariableTable.xlsx']);
-% writetable(allSubjStatTable, fullResultFile, 'WriteVariableNames',true)
-end
-
-function [summaryStr, variableStat] = RawSummaryStats(variableName, measure, idealLambda)
-
-numObs    = length(measure);
-
-% Calculate the Descriptive Stats
-summaryStr.varName  = variableName;
-summaryStr.measure  = measure;        % Raw Data Values
-
-summaryStr.mean     = round(mean(measure), 2);
-summaryStr.median   = round(median(measure), 2);
-summaryStr.min      = round(min(measure), 2);
-summaryStr.max      = round(max(measure), 2);
-summaryStr.SD       = round(std(measure), 2);
-summaryStr.SE       = round(summaryStr.SD/sqrt(numObs), 2);
-
-summaryStr.isTrans     = 0;       % Default is not transformed
-summaryStr.measureT    = measure; % Transformed Data Values (Default is the same)
-summaryStr.measureZ    = [];      % Z-Scored Data Values
-summaryStr.idealLambda = idealLambda;
-summaryStr.usedLambda  = 'N/A';   % Default is not transformed
-summaryStr.suffix      = '';      % Default is not transformed
-
-variableStat = {summaryStr.mean;...
-                summaryStr.min;...
-                summaryStr.median;...
-                summaryStr.max;...
-                summaryStr.SD;...
-                summaryStr.SE};
-                  
-
-end
-
-function summaryStr = testNormality(summaryStr)
-
-% Skew and Kurtosis
-summaryStr.measureSkew     = round(skewness(summaryStr.measureT), 4);
-summaryStr.measureKurtosis = round(kurtosis(summaryStr.measureT), 2);
-
-% Z-Score and Shapiro-Wilk Test
-summaryStr.measureZ        = zscore(summaryStr.measureT);
-[swH, swPValue, swTest]    = swtest(summaryStr.measureZ);
-
-summaryStr.swH      = double(swH);
-summaryStr.swPValue = round(swPValue, 3);
-summaryStr.swTest   = round(swTest, 3);
-end
-
-function variableStat = concateAdditionalStat(summaryStr, variableStat)
-
-variableStat = cat(1, variableStat, summaryStr.measureSkew);
-variableStat = cat(1, variableStat, summaryStr.measureKurtosis);
-variableStat = cat(1, variableStat, summaryStr.swH);
-variableStat = cat(1, variableStat, summaryStr.swPValue);
-variableStat = cat(1, variableStat, summaryStr.swTest);
 end
 
 function drawHistograms(measureSummaryStrs, dirs, pA)
@@ -261,6 +186,7 @@ xt = get(gca, 'XTick');
 numComp = length(summaryStrDiff);
 for ii = 1:numComp
     cS = summaryStrDiff(ii);
+    pVal = cS.ttestPstr;
     barR = xt(cS.vars);
     barM = mean(barR);
     if cS.isSig == 1
@@ -268,7 +194,7 @@ for ii = 1:numComp
         plot(barR, [1 1]*max(yt)*(cS.h+.01), '-k')
         plot(barM, max(yt)*(cS.h+.02), '*k', 'MarkerSize', 10)
     end
-    text(barM-0.35, max(yt)*(cS.h+.05), ['p = ' cS.fPround], 'FontSize',18)
+    text(barM-0.35, max(yt)*(cS.h+.05), ['p = ' pVal], 'FontSize',18)
 end
 set(gca,'FontName', fontN,...
         'FontSize', axisLSize,...
@@ -276,55 +202,4 @@ set(gca,'FontName', fontN,...
 
 dirs.BoxPlotFigureFile = fullfile(dirs.SavResultsDir, 'Question4BoxPlot.jpg');
 export_fig(dirs.BoxPlotFigureFile)
-end
-
-function drawHistoBoxCombo(summaryStr, dirs, pA)
-
-measure = summaryStr.measureT;
-varName = summaryStr.varName;
-suffix  = summaryStr.suffix;
-swH = summaryStr.swH; swP = summaryStr.swPValue; swW = summaryStr.swTest;
-
-pAnalysis = pA.pAnalysis;
-lambda = '\lambda';
-
-diffBox = figure('Color', [1 1 1]);
-plotpos = [30 0]; plotdim = [800 300];
-set(diffBox, 'Position',[plotpos plotdim],'PaperPositionMode','auto')
-
-subplot(1,2,1); histogram(measure, 10); box off
-title(['H=' num2str(swH) ', p=' num2str(round(swP,4)) ', W=' num2str(round(swW,3))])
-
-subplot(1,2,2); boxplot(measure); box off
-suptitle(varName)
-
-annotation('textbox',[0.8 0.88 0.45 0.1],...
-           'string', {[lambda ' = ' summaryStr.usedLambda]},...
-           'LineStyle','none',...
-            'FontWeight','bold',...
-            'FontSize',14,...
-            'FontName','Arial');
-
-dirs.BoxPlotFigureFile = fullfile(dirs.SavResultsDir, [pAnalysis varName suffix 'BoxPlotCombo.jpg']);
-export_fig(dirs.BoxPlotFigureFile)
-end
-
-function [rAnovaRes, measSph] = testParametric(curStatTable, cond_table)
-
-condTable = table(cond_table');
-
-measFit = fitrm(curStatTable, 'VoiceFeedback-AC_BCMaskingNoise~1', 'WithinDesign', condTable, 'WithinModel', 'separatemeans');
-measSph = mauchly(measFit);
-    
-rAnovaRes = ranova(measFit);
-end
-
-function [tFried] = testNonParametric(measureSummaryStrs)
-
-measuresColl = [];
-for i = 1:3
-    measuresColl = cat(2, measuresColl, measureSummaryStrs(i).measureT);
-end
-
-[pFried,tFried, stats] = friedman(measuresColl);
 end
