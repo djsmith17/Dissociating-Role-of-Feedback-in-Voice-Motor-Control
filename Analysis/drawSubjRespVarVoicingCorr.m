8a845dca6b099489af5dd417f5b71f5a6a4c5f1c
djsmith17 <djsmith@bu.edu>
2019-07-03 15:33:43 -0400

    Normalizing the RespPer values for corr with voicing time
    and making an all participant version.

--- Analysis/drawSubjRespVarVoicingCorr.m
+++ Analysis/drawSubjRespVarVoicingCorr.m
@@ -15,6 +15,8 @@ ha = tight_subplot(3, 6, [0.08 0.03],[0.06 0.15],[0.05 0.03]);
 pA.pAnalysis(strfind(pA.pAnalysis, '_')) = '';
 scattColors = {'b', 'r'};
 
+fullRespPer = cell(pA.numCond, 1);
+fullVTimes  = cell(pA.numCond, 1);
 for ii = 1:numSubj
     axes(ha(ii))
      
@@ -28,28 +30,33 @@ for ii = 1:numSubj
         respPer      = curRes.respVarSingle{jj}.respPer;
         voicingTimes = curRes.prePertVoicingTimePert{jj};
 
-%         wayWrong = respPer > 300 | respPer < -300;
-%         respPer      = respPer(~wayWrong);
-%         voicingTimes = voicingTimes(~wayWrong);
-%         
+        wayWrong = respPer > 300 | respPer < -300;
+        respPer      = respPer(~wayWrong);
+        voicingTimes = voicingTimes(~wayWrong);
+
+        respPerNorm = NormalizeByMax(respPer);
+    
         numTrial = length(respPer);
 
-        varToCorr = [voicingTimes, respPer];
+        varToCorr = [voicingTimes, respPerNorm];
         [corrR, corrP] = corrcoef(varToCorr);
 
-        plot(voicingTimes, respPer, 'o', 'color', scattColors{jj}, 'MarkerSize', 5)
+        plot(voicingTimes, respPerNorm, 'o', 'color', scattColors{jj}, 'MarkerSize', 5)
         hold on
         
-        allRespPer = [allRespPer; respPer];
+        allRespPer = [allRespPer; respPerNorm];
         allTimes   = [allTimes; voicingTimes];
+        
+        fullRespPer{jj} = [fullRespPer{jj}; respPerNorm];
+        fullVTimes{jj}  = [fullVTimes{jj}; voicingTimes];
         
         statSentence = sprintf('r = %0.2f, p = %0.3f, n = %d\n', corrR(1,2), corrP(1,2), numTrial);
         allStatSentence = cat(1, allStatSentence, statSentence);
     end
     respPerMin = min(allRespPer);
     respPerMax = max(allRespPer);
-    lowB = respPerMin - 10;
-    upB  = respPerMax + 50;
+    lowB = respPerMin - 0.25;
+    upB  = respPerMax + 1;
     
     timesMin = min(allTimes);
     timesMax = max(allTimes);
@@ -87,4 +94,74 @@ suptitle(pA.pAnalysis)
 pltTitle = [pA.pAnalysis 'IndividualParticipantVoicingRespPerCorr.jpg'];
 plotFileName = fullfile(dirs.SavResultsDir, pltTitle);
 export_fig(plotFileName)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+plotpos = [10 40];
+plotdim = [1800 900];
+AllTrialVTimingCorr = figure('Color', [1 1 1]);
+set(AllTrialVTimingCorr, 'Position',[plotpos plotdim],'PaperPositionMode','auto')
+
+allRespPerBounds = [];
+allVTimesBounds = [];
+lgdNames = {};
+for jj = 1:pA.numCond
+    respPer = fullRespPer{jj};
+    vTimes  = fullVTimes{jj};
+    
+    allRespPerBounds = [allRespPerBounds; max(respPer)];
+    allRespPerBounds = [allRespPerBounds; min(respPer)];
+        
+    allVTimesBounds = [allVTimesBounds max(vTimes)];
+    allVTimesBounds = [allVTimesBounds min(vTimes)];
+    
+    numTrial = length(respPer);
+    
+    varToCorr = [vTimes, respPer];
+    [corrR, corrP] = corrcoef(varToCorr);
+    
+    plot(vTimes, respPer, 'o', 'color', scattColors{jj}, 'MarkerSize', 5)
+    hold on
+    
+    statSentence = sprintf('r = %0.2f, p = %0.3f, n = %d\n', corrR(1,2), corrP(1,2), numTrial);
+    lgdNames = cat(1, lgdNames, statSentence);
+end
+respPerMin = min(allRespPerBounds);
+respPerMax = max(allRespPerBounds);
+lowB = respPerMin - 0.25;
+upB  = respPerMax + 0.25;
+
+timesMin = min(allVTimesBounds);
+timesMax = max(allVTimesBounds);
+leftB = timesMin - 0.1;
+rightB = timesMax + 0.1;
+
+xlabel('Pre Pert Voicing Time (s)')
+ylabel('Normalized RespPer (%)')
+title(pA.pAnalysis)
+
+box off
+axis([leftB rightB lowB upB])
+
+set(gca, 'FontName', fontN,...
+         'FontSize', axisLSize,...
+         'FontWeight','bold')
+
+
+legend(lgdNames,...
+    'EdgeColor', 'none',...
+    'FontSize', 15,...
+    'Location', 'northwest')
+
+pltTitle = [pA.pAnalysis 'AllParticipantVoicingRespPerCorr.jpg'];
+plotFileName = fullfile(dirs.SavResultsDir, pltTitle);
+export_fig(plotFileName)
+end
+
+function NormVec = NormalizeByMax(vector)
+
+maxVal = max(vector);
+
+NormVec = vector/maxVal;
+
 end
