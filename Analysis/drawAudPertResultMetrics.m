11a38f5b09051c2a5975205769b12fab9a2acfd6
djsmith17 <djsmith@bu.edu>
2019-09-04 01:06:01 -0400

    Updated Exemplary figure plots
    Updating the figures that exemplify what the recorded f0 signals look like.

--- Analysis/drawAudPertResultMetrics.m
+++ Analysis/drawAudPertResultMetrics.m
@@ -1,10 +1,10 @@
 function drawAudPertResultMetrics(ir, arrows, vals)
-% This has been optimized to diagram DRF4 AF1 mean trace results.
+% This has been optimized to diagram DRF19 AF2 mean trace results.
 % Hopefully in the future, the arrows will 
 
 dim     = 50;
 plotPos = [10 40];
-plotDim = 3*[500 500];
+plotDim = [1485 1005];
 
 fontN        = 'Arial';
 lineThick    = 3;
@@ -26,13 +26,13 @@ plot([-0.6 1.1], [ir.vAtOnset, ir.vAtOnset], 'r--', 'LineWidth', 2.5)
 % Draw the pitch trace
 plot(ir.time, ir.onset, 'k', 'LineWidth', lineThick)
 xlabel('Time (s)')
-ylabel('f0 (Cents)')
+ylabel('{\it f}_o (Cents)')
 hold on
 
 plot(ir.tAtOnset, ir.vAtOnset, 'Color', onsetC, 'Marker', 's', 'MarkerFaceColor',onsetC, 'MarkerSize', 20)
 plot(ir.tAtResp, ir.vAtResp, 'Color', respC, 'Marker', 'o', 'MarkerFaceColor', respC,'MarkerSize', 20);
 
-axis([ir.time(1) ir.time(end) (min(ir.onset) - 20) (max(ir.onset) + 20)])
+axis([ir.time(1) ir.time(end) (min(ir.onset) - 10) (max(ir.onset) + 10)])
 box off
 
 set(gca,'LineWidth', 2,...
@@ -45,11 +45,12 @@ set(gca,'LineWidth', 2,...
 if arrows == 1
 
     RMArrowX = [0.905 0.905];
-    RMArrowY = [0.255 0.79];
+    RMArrowY = [0.25 0.775];
     annotation('arrow', RMArrowX, RMArrowY, 'LineWidth', 5, 'HeadWidth', 20)
-    annotation('textbox', [0.760 0.3 0.1 0.1],...
-                         'string', 'RespMag',...
+    annotation('textbox', [0.77 0.33 0.1 0.1],...
+                         'string', {'Response'; 'Magnitude'},...
                          'LineStyle', 'none',...
+                         'HorizontalAlignment', 'center',...
                          'FontWeight','bold',...
                          'FontName', fontN,...
                          'FontSize',legAnnoFSize)
@@ -74,8 +75,8 @@ if vals == 1
                              'FontWeight','bold');
 end
                      
-% path = 'E:\Desktop';
-path  = 'C:\Users\djsmith\Desktop';
+path = 'E:\Desktop';
+% path  = 'C:\Users\djsmith\Desktop';
 fileName = 'AudDependentVariablesManuscript.png';
 fullpath = fullfile(path, fileName);
 export_fig(fullpath)
